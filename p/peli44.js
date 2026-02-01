d5f2695cf1a1ad281f3b733f71f94469534bd156
Auraflix <lestheradonys@gmail.com>
2026-01-31 20:31:08 -0600

    Add files via upload

---
+++ p/peli44.js
@@ -0,0 +1,13 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p44",
+    title: "Ne Zha 2: El Renacer del Alma",
+    image: "https://raw.githubusercontent.com/yoldkwlws/Stranger-things/refs/heads/main/cb5NyNrqiCNNoDkA8FfxHAtypdG.jpg",
+    trailer: "https://www.youtube.com/embed/2otR4GFP03I",
+    video: "https://vimeos.net/embed-5op98hottk57.html",
+    year: 2025,
+    genres: ["Animación", "Fantasía", "Aventura", "Acción"],
+    info: "Tras los eventos del primer filme, Ne Zha y Ao Bing deben enfrentar las consecuencias de sus actos mientras una nueva amenaza surge de las profundidades. Con el equilibrio entre el cielo y la tierra en peligro, Ne Zha deberá embarcarse en un viaje espiritual para dominar su verdadero poder y descubrir el secreto detrás del renacer de las almas en un mundo lleno de mitología y batallas épicas.",
+    featured: true
+});
