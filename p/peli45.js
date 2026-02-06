9d99527a863c9acd7ec7c0c7bbc788c804899845
Auraflix <lestheradonys@gmail.com>
2026-02-05 23:15:15 -0600

    Add files via upload

---
+++ p/peli45.js
@@ -0,0 +1,13 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p45",
+    title: "Bugonia",
+    image: "https://raw.githubusercontent.com/yoldkwlws/Stranger-things/refs/heads/main/9qkNGG8pfcZFJd5jBUi5NOIhWXe.jpg",
+    trailer: "https://www.youtube.com/embed/ugTqv5T0-2s",
+    video: "https://vimeos.net/embed-omaxerxskfus.html",
+    year: 2025,
+    genres: ["Ciencia ficción", "Suspenso", "Comedia"],
+    info: "Dos hombres obsesionados con las teorías de la conspiración deciden secuestrar a la poderosa directora ejecutiva de una gran corporación. Están convencidos de que ella es, en realidad, una alienígena que planea la destrucción de la Tierra. Lo que comienza como un intento desesperado por salvar a la humanidad se convierte en una sátira oscura y extraña sobre la paranoia y la verdad.",
+    featured: true
+});
---
+++ p/peli46.js
@@ -0,0 +1,14 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p46",
+    title: "Trol 2",
+    tags: ["Troll 2"],
+    image: "https://raw.githubusercontent.com/yoldkwlws/Stranger-things/refs/heads/main/1rOY8NCGEpOGVQ2EzMJb6B7Ni6X.jpg",
+    trailer: "https://www.youtube.com/embed/lJS2WyZfB44",
+    video: "https://vimeos.net/embed-mmwf2qetgghv.html",
+    year: 2025,
+    genres: ["Acción", "Fantasía", "Suspenso"],
+    info: "Cuando un nuevo y peligroso Megatrol de proporciones épicas despierta en el Ártico y amenaza con devastar Noruega, la paleontóloga Nora Tidemann debe reunirse con Andreas y el capitán Kris para detener la catástrofe. En una carrera contra el tiempo, descubrirán que la historia oficial de la Segunda Guerra Mundial ocultaba una verdad mitológica mucho más oscura, obligándolos a enfrentar una amenaza que las luces ultravioletas ya no pueden contener.",
+    featured: true
+});
