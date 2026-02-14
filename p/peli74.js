769350902a0baebda7135ecbd80e3419f108d662
Auraflix <lestheradonys@gmail.com>
2026-02-13 22:56:59 -0600

    Add files via upload

---
+++ p/peli74.js
@@ -0,0 +1,14 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p74",
+    title: "Una casa llena de dinamita",
+    tags: ["A House of Dynamite", "Una casa de dinamita"],
+    image: "https://raw.githubusercontent.com/Auraflixs/Stranger-things/refs/heads/main/mkpV4oNsAF2OTlnlCnmJVN838Tf.jpg",
+    trailer: "https://www.youtube.com/embed/_wpw2QHJNco",
+    video: "https://vimeos.net/embed-s08cajihilm7.html",
+    year: 2025,
+    genres: ["Suspenso"],
+    info: "Un thriller de tensión constante donde un robo que parecía sencillo se complica cuando los delincuentes descubren que la casa en la que han irrumpido está cargada de explosivos por un propietario paranoico. Atrapados en una trampa mortal, deben colaborar con sus rehenes para encontrar una salida antes de que un solo movimiento en falso haga volar todo por los aires.",
+    featured: true
+});
---
+++ p/peli75.js
@@ -0,0 +1,14 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p75",
+    title: "La mujer del camarote 10",
+    tags: ["The Woman in Cabin 10"],
+    image: "https://raw.githubusercontent.com/Auraflixs/Stranger-things/refs/heads/main/y4LrRDNex3qH7LHDr2kZYrKf50v.jpg",
+    trailer: "https://www.youtube.com/embed/1QbfJzTPY4w",
+    video: "https://vimeos.net/embed-mlmphyiz525l.html",
+    year: 2025,
+    genres: ["Misterio", "Suspenso", "Crimen"],
+    info: "Una periodista de viajes es invitada a un crucero de lujo para cubrir su inauguración. Sin embargo, su viaje de ensueño se convierte en una pesadilla cuando presencia cómo una mujer es arrojada por la borda. El misterio se intensifica cuando se reporta que todos los pasajeros están presentes y que nadie ocupaba el camarote 10, obligándola a cuestionar su propia realidad mientras busca la verdad en alta mar.",
+    featured: true
+});
