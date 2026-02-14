80f407865e69eea9b0a7e716606e1b252ce8aa5e
Auraflix <lestheradonys@gmail.com>
2026-02-13 20:53:22 -0600

    Add files via upload

---
+++ p/peli73.js
@@ -0,0 +1,14 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p73",
+    title: "Los Extraños: Capítulo 3",
+    tags: ["The Strangers: Chapter 3"],
+    image: "https://raw.githubusercontent.com/Auraflixs/Stranger-things/refs/heads/main/yPegTig8dQNYiW2UW21LTNChGkK.jpg",
+    trailer: "https://www.youtube.com/embed/B2fZEIHamVs",
+    video: "https://dropload.pro/e/7cbpapknp2sq",
+    year: 2026,
+    genres: ["Terror", "Suspenso"],
+    info: "En el capítulo final de esta trilogía de terror, se revelan los oscuros motivos detrás de los enmascarados mientras la última superviviente se ve obligada a enfrentarse a su pasado para poner fin a la pesadilla. Un cierre brutal que expande el universo de 'Los Extraños' y responde a la pregunta definitiva: ¿Por qué ellos?",
+    featured: true
+});
