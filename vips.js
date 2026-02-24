5f07dc01a19275678adcaeb953c8fe930d448e42
Auraflix <lestheradonys@gmail.com>
2026-02-23 20:22:30 -0600

    Add files via upload

---
+++ vips.js
@@ -0,0 +1,17 @@
+// Archivo de enlaces de acortadores para Series
+window.vipsLinks = [
+    { 
+        id: "s2", 
+        season: 3, 
+        episode: 1, 
+        link: "https://acortador.com/serie23-t3-e1" 
+        // 👆 Este link acortado debe llevar finalmente a: https://auraflixs.github.io/?serie=s23-t3-e1-vip
+    },
+    { 
+        id: "s1", 
+        season: 3, 
+        episode: 1, 
+        link: "https://acortador.com/serie23-t3-e2" 
+    }
+    // Añade más aquí separando por comas...
+];
