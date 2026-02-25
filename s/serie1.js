6ca014a30e6e9d49ffb6edf387d9661b33b9a2d0
Auraflix <lestheradonys@gmail.com>
2026-02-24 19:56:03 -0600

    Add files via upload

---
+++ s/serie1.js
@@ -0,0 +1,60 @@
+window.seriesList = window.seriesList || [];
+
+window.seriesList.push({
+    id: "s1",
+    title: "Monstruo: La historia de Ed Gein",
+    tags: ["MONSTER: The Ed Gein Story"],
+    year: "2025",
+    genres: ["Drama", "Crimen"],
+    image: "https://raw.githubusercontent.com/Auraflixs/im-genestv/refs/heads/main/1v9X7qviwTllikhIZdVAjRmDQtL.webp",
+    trailer: "https://www.youtube.com/embed/J20xarD-SxI?start=44",
+    info: "Wisconsin, años 50. Tras la muerte de su dominante madre, el solitario granjero Ed Gein comienza a hundirse en una espiral de locura y necrofilia. Lo que comienza como profanaciones de tumbas pronto se convierte en una serie de asesinatos atroces que conmocionan a la nación, revelando una colección macabra de trofeos humanos en su granja de Plainfield que cambiaría la historia de la criminología para siempre.",
+    featured: true,
+    seasons: [
+        {
+            season: 1,
+            episodes: [
+                { 
+                    episode: 1, 
+                    video: "https://dropload.pro/e/2xxmrkoet8sq",
+                    unlockLink: "https://direct-link.net/2492660/R7nL3viWQ1QA"
+                },
+                { 
+                    episode: 2, 
+                    video: "https://dropload.pro/e/bq0gnnbk2enh",
+                    unlockLink: "https://link-target.net/2492660/Rq3BuGR6bOZQ" 
+                },
+                { 
+                    episode: 3, 
+                    video: "https://dropload.pro/e/ef8ckwndafuw",
+                    unlockLink: "https://link-target.net/2492660/27qwbODz5vDJ" 
+                },
+                { 
+                    episode: 4, 
+                    video: "https://dropload.pro/e/af5friq15uxf",
+                    unlockLink: "https://link-center.net/2492660/7KIxDNkYswLb" 
+                },
+                { 
+                    episode: 5, 
+                    video: "https://dropload.pro/e/j3eonvqmrl63",
+                    unlockLink: "https://link-center.net/2492660/w0znlY4EEZIQ" 
+                },
+                { 
+                    episode: 6, 
+                    video: "https://dropload.pro/e/238cc64zmdca",
+                    unlockLink: "https://link-center.net/2492660/bjGFYv1RHW8a" 
+                },
+                { 
+                    episode: 7, 
+                    video: "https://dropload.pro/e/3h72d1bhfcal",
+                    unlockLink: "https://link-hub.net/2492660/EviDNnIAZoNN" 
+                },
+                { 
+                    episode: 8, 
+                    video: "https://dropload.pro/e/kw1x6hii3fsu",
+                    unlockLink: "https://link-hub.net/2492660/o6atRLtsXTLY" 
+                }
+            ]
+        }
+    ]
+});
