b6f964776b6b414749878b6c909901fa432e0198
Auraflix <lestheradonys@gmail.com>
2026-02-26 13:37:19 -0600

    Add files via upload

---
+++ p/peli1.js
@@ -0,0 +1,14 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p1",
+    title: "Frankenstein",
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/rJjNMlxbjO34myMlzZJAOORBg2O.webp",
+    trailer: "https://www.youtube.com/embed/11j2wQFm-tc?start=28",
+    video: "https://dropload.pro/e/kagle2z1i15u",
+    year: 2025,
+    genres: ["Drama", "Fantasía", "Terror"],
+    info: "El visionario Guillermo del Toro da vida a esta nueva adaptación del clásico de Mary Shelley. El Dr. Victor Frankenstein continúa su ambicioso experimento para desafiar a la muerte, creando una criatura que deberá enfrentar su propia humanidad en un mundo que la teme.",
+    featured: true,
+    unlockLink: "https://direct-link.net/2492660/EdkpQ66KnUI7"
+});
