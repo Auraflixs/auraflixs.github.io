ded96f652ca95d95d1042bb78ccc5782e172baed
Auraflix <lestheradonys@gmail.com>
2026-03-19 11:59:40 -0600

    Add files via upload

---
+++ cate.js
@@ -0,0 +1,12 @@
+// cate.js
+window.customCategories = [
+    {
+        title: "El Universo del Conjuro", // El título que saldrá en la pantalla
+        ids: ["p6", "p54", "p55", "p56"] // Reemplaza estos con los IDs reales de tus películas/series
+    },
+    {
+        title: "los ilucionistas",
+        ids: ["p49", "p50", "p51"]
+    }
+    // Puedes agregar todas las categorías que quieras copiando y pegando el bloque de arriba
+];
