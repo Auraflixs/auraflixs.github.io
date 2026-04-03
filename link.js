f7b961be3a446d1146629145a51e9718efed75f3
Auraflix <lestheradonys@gmail.com>
2026-04-02 21:48:17 -0600

    Update link.js

--- link.js
+++ link.js
@@ -3,7 +3,7 @@ const AURA_LINKS = {
     telegram: 'https://t.me/Auraflixpeli',
 
     // Enlace para descargar la App (Botón Compartir App y Actualizar)
-    appDownload: 'https://github.com/Auraflixs/c-digo-vip/raw/refs/heads/main/app-apk-699dc989762c8-1771948425.apk',
+    appDownload: 'https://www.mediafire.com/file/yg2vwae5m91sv3s/Xuper+TV+Premium+v6.2.2+-+androforever.com.apk/file',
     // Enlace de la página Web (Botón Compartir Web)
     webPage: 'https://auraflixs.github.io'
 };
