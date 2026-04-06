2c91f4bfcf03e15978d63c04d0abf0ae21b1a9e2
Auraflix <lestheradonys@gmail.com>
2026-04-06 17:12:04 -0600

    Update peli78.js

--- p/peli78.js
+++ p/peli78.js
@@ -5,7 +5,7 @@ window.moviesList.push({
     title: "Hamnet",
     image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/vbeyOZm2bvBXcbgPD3v6o94epPX.webp",
     trailer: "https://www.youtube.com/embed/Ith7z6FzLe0",
-    video: "https://streamtape.com/v/KZz3rj6jJ7f0q6b/Hamnet_720p_DUAL_id_858024.mkv.mp4",
+    video: "https://streamtape.com/e/KZz3rj6jJ7f0q6b/Hamnet_720p_DUAL_id_858024.mkv.mp4",
     year: 2025,
     genres: ["Drama", "Romance", "Historia"],
     info: "Ambientada en la Inglaterra de 1580, la película explora la vida de Agnes, la esposa de William Shakespeare, y el profundo dolor que envuelve a su familia tras la pérdida de su hijo Hamnet a causa de la peste. Una historia conmovedora sobre el duelo, la creación artística y el amor que inspiraría una de las obras más famosas de la literatura universal.",
