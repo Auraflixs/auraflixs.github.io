8a807e75b6695673dbccb29aaeb8bbd3a401b587
Auraflix <lestheradonys@gmail.com>
2026-04-06 17:40:02 -0600

    Add files via upload

---
+++ p/peli79.js
@@ -0,0 +1,14 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p79",
+    title: "Scream 7",
+    year: "2026",
+    genre: "Terror, Misterio, Suspenso",
+    tags: ["Scream VII", "Grito 7"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/jjyuk0edLiW8vOSnlfwWCCLpbh5.webp",
+    trailer: "https://www.youtube.com/embed/4g8OciWNJn4",
+    video: "https://streamtape.com/e/kgJe0Y0mvkUOl7m/Scream_7_720p_TRIAL_id_1159559.mkv",
+    info: "Ghostface regresa una vez más para aterrorizar a los supervivientes en esta séptima entrega de la franquicia. Ambientada en un entorno urbano masivo, la película profundiza en las reglas del cine de terror moderno mientras los protagonistas intentan desenmascarar al asesino antes de convertirse en las próximas víctimas.",
+    featured: true
+});
