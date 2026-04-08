2b99c8dd74e0246523770db9ef1ff8cdbaad81d8
Auraflix <lestheradonys@gmail.com>
2026-04-07 18:42:38 -0600

    Add files via upload

---
+++ p/peli81.js
@@ -0,0 +1,14 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p81",
+    title: "El gran diluvio",
+    year: "2025",
+    genre: "Acción, Aventura, Ciencia ficción",
+    tags: ["The Great Flood", "Diluvio"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/qIzSyT2AmDxkcQ0zOAeTbtZR9EQ.webp",
+    trailer: "https://www.youtube.com/embed/avtunpP2a9M",
+    video: "https://streamtape.com/e/r383bRe4PXHbzAa/El_gran_diluvio_720p_DUAL_id_982843.mkv",
+    info: "En el último día de la Tierra, una inundación catastrófica amenaza con borrar a la humanidad. Mientras el mundo se sumerge bajo el agua, la lucha por la supervivencia se vuelve desesperada en una carrera contra el tiempo y la naturaleza indomable.",
+    featured: true
+});
