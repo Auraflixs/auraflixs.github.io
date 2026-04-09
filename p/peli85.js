4e3a5cfaa8a6d1a4081cf635bef8f541b87767e4
Auraflix <lestheradonys@gmail.com>
2026-04-09 13:54:09 -0600

    Add files via upload

---
+++ p/peli85.js
@@ -0,0 +1,14 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p85",
+    title: "¡La novia!",
+    year: "2026",
+    genre: "Terror, Ciencia ficción, Drama",
+    tags: ["The Bride"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/4xC5IfnCekweHkTFjqQsMOC1gca.webp",
+    trailer: "https://www.youtube.com/embed/IhgcUArO3Uo",
+    video: "https://streamtape.com/e/bweD7looK1UPgOR/%C2%A1La_novia%21_2026_1080p_DUAL_id_1159831.mkv",
+    info: "En el Chicago de los años 30, un solitario monstruo de Frankenstein le pide ayuda al Dr. Euphronius para crear una compañera. Juntos reviven a una joven asesinada, dando origen a la Novia, cuya presencia desata una evolución radical, un romance inesperado y el caos en la ciudad.",
+    featured: true
+});
