b0014631b7a913525ca5e73c7a49965e13286715
Auraflix <lestheradonys@gmail.com>
2026-04-09 16:09:36 -0600

    Add files via upload

---
+++ p/peli88.js
@@ -0,0 +1,14 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p88",
+    title: "El Guardián: Último Refugio",
+    year: "2026",
+    genre: "Acción, Suspenso, Crimen",
+    tags: ["Shelter", "Jason Statham", "SHELTER: EL PROTECTOR"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/qf3fKeFuhidYlN2b1JA4P7g2tXD.webp",
+    trailer: "https://www.youtube.com/embed/auHiLFjaIc0",
+    video: "https://streamtape.com/e/brPYg8zXzaFJeZ/Shelter_El_Protector.mp4",
+    info: "Michael Mason, un exoperativo de élite que vive en aislamiento, se ve obligado a abandonar su retiro para proteger a una joven que termina en el lugar equivocado en el momento equivocado. Perseguido por fuerzas corruptas y asesinos implacables, deberá usar todas sus habilidades de combate para sobrevivir y garantizar la seguridad de su protegida.",
+    featured: true
+});
