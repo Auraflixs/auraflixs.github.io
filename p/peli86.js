6c046841f17b0bfda480389db3e2443c60882fa8
Auraflix <lestheradonys@gmail.com>
2026-04-09 14:13:52 -0600

    Add files via upload

---
+++ p/peli86.js
@@ -0,0 +1,14 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p86",
+    title: "Pura energía",
+    year: "1995",
+    genre: "Ciencia ficción, Drama, Misterio",
+    tags: ["Powder"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/m6NwG7vPRyqpfVin2oP0h7dPmDl.webp",
+    trailer: "https://www.youtube.com/embed/lGp4WXcVEno",
+    video: "https://streamtape.com/e/aql0yyW3Kzhx49V/Pura_Energia_720p_LAT_id_12665.mp4",
+    info: "Un joven albino apodado 'Powder' posee una inteligencia sobrehumana y extraños poderes eléctricos. Tras vivir toda su vida oculto, es descubierto y llevado a la sociedad, donde su presencia desafía todo lo que la ciencia y la comunidad creen saber sobre la naturaleza humana.",
+    featured: true
+});
---
+++ p/peli87.js
@@ -0,0 +1,14 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p87",
+    title: "Him: El elegido",
+    year: "2025",
+    genre: "Terror, Misterio, Suspenso",
+    tags: ["El mejor"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/n2xPRI0isWxKfX3UqlfnNPaPusg.webp",
+    trailer: "https://www.youtube.com/embed/hpEy0iOixb4",
+    video: "https://streamtape.com/e/owpG94V8AwtJXAG/HIM_-_El_elegido_DUAL_1080p_id_986097.mkv",
+    info: "Un joven y prometedor mariscal de campo es invitado a entrenar en las instalaciones aisladas de una leyenda del fútbol americano. Sin embargo, lo que comienza como una oportunidad única para alcanzar la gloria se convierte en una pesadilla psicológica y violenta donde el precio del éxito podría ser su propia cordura.",
+    featured: true
+});
