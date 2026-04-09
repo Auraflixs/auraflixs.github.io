6c9c66e99515f186d8e91ba4f1c953c11e8cc43f
Auraflix <lestheradonys@gmail.com>
2026-04-09 13:19:08 -0600

    Update linkvip.js

--- linkvip.js
+++ linkvip.js
@@ -1,4 +1,13 @@
 window.vipLinksList = [
     "https://lnbz.la/dia1",
-    "https://lnbz.la/codigo_1"
+    "https://lnbz.la/codigo_1",
+    "https://lnbz.la/dia2",
+    "https://lnbz.la/dia3",
+    "https://lnbz.la/dia4",
+    "https://lnbz.la/dia5",
+    "https://lnbz.la/dia6",
+    "https://lnbz.la/dia7",
+    "https://lnbz.la/dia8",
+    "https://lnbz.la/dia9",
+    "https://lnbz.la/dia10",
 ];
