5da8e6cecbbdea418179db4f9bff5ac452cc8e5d
Auraflix <lestheradonys@gmail.com>
2026-04-08 22:33:02 -0600

    Add files via upload

---
+++ p/peli82.js
@@ -0,0 +1,14 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p82",
+    title: "Proyecto Salvación",
+    year: "2026",
+    genre: "Ciencia ficción, Drama, Aventura",
+    tags: ["Project Hail Mary", "Proyecto Fin del mundo"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/7lwOTxajURKEWO6gI370NTrVdBO.webp",
+    trailer: "https://www.youtube.com/embed/in-lUuKi0eE",
+    video: "https://streamtape.com/e/oe6Kr9xlqaIJzgr/Proyecto_Fin_del_mundo_-_1.mp4",
+    info: "Ryland Grace es el único superviviente en una misión desesperada por salvar la humanidad y la Tierra. Sin recuerdos de quién es ni de su tarea, deberá usar la ciencia y a un inesperado aliado para evitar la extinción de ambos mundos.",
+    featured: true
+});
