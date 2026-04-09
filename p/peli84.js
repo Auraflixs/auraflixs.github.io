67b87cbc38942b845e0893ea18862d0a1bab1941
Auraflix <lestheradonys@gmail.com>
2026-04-09 12:48:01 -0600

    Add files via upload

---
+++ p/peli84.js
@@ -0,0 +1,13 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p84",
+    title: "Hoppers: operación castor",
+    year: "2026",
+    genre: "Animación, Aventura, Familia",
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/12tvpCv413QvvJlZGf4lRq446tT.webp",
+    trailer: "https://www.youtube.com/embed/bv0sKzFvpmo",
+    video: "https://streamtape.com/e/JbpPx9Y9oxIezG/Hoppers__Operaci%C3%B3n_castor_-_1.mp4",
+    info: "Mabel, una joven amante de los animales, utiliza una tecnología revolucionaria para transferir su conciencia a un castor robótico. En su misión por entender el mundo animal, termina convirtiéndose en una líder rebelde que debe unir a las criaturas del estanque para proteger su hogar de la amenaza humana.",
+    featured: true
+});
