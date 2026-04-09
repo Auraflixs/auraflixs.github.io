739134b70aacf27c1e771be2945f771c562c1c81
Auraflix <lestheradonys@gmail.com>
2026-04-09 17:01:39 -0600

    Add files via upload

---
+++ p/peli90.js
@@ -0,0 +1,14 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p90",
+    title: "Hogar Dulce Hogar: Renacimiento",
+    year: "2025",
+    genre: "Terror, Fantasía, Suspenso",
+    tags: ["Home Sweet Home: Rebirth"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/9rCBCm9cyI4JfLEhx3EncyncMR3.webp",
+    trailer: "https://www.youtube.com/embed/-17tBEdfDIs",
+    video: "https://streamtape.com/e/JkpXdLxoA2TjdXj/Home_Sweet_Home__Rebirth_-_1.mp4",
+    info: "Jake, un policía, se ve transportado a una realidad alterna conocida como 'el Hindrance' mientras investiga una serie de desapariciones. Allí descubre que es el guardián de las puertas y el único con el poder de cerrar los portales del infierno para salvar a su familia y detener a las fuerzas demoníacas que amenazan con consumir el mundo.",
+    featured: true
+});
