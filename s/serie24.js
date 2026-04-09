6c7fe24c58a50b25079267db26307f9c34e93c1e
Auraflix <lestheradonys@gmail.com>
2026-04-09 10:55:32 -0600

    Add files via upload

---
+++ s/serie24.js
@@ -0,0 +1,48 @@
+window.seriesList = window.seriesList || [];
+
+window.seriesList.push({
+    id: "s24",
+    title: "Scream Queens",
+    year: "2015",
+    genre: "Terror, Comedia, Misterio",
+    tags: ["Reinas del grito"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/yeayXZYSU8xdmC8i5g5jTdxeggp.webp",
+    trailer: "https://www.youtube.com/embed/wufDF80AgBg",
+    info: "La fraternidad Kappa Kappa Tau se ve sacudida por una serie de asesinatos cometidos por un asesino disfrazado de Red Devil. Un grupo de estudiantes deberá descubrir la identidad del culpable antes de que sea demasiado tarde.",
+    featured: true,
+    seasons: [
+        {
+            season: 1,
+            episodes: [
+                { episode: 1, video: "https://streamtape.com/e/bZeeB2jgKktPwra/Scream_Queens_S01E01.mkv" },
+                { episode: 2, video: "https://streamtape.com/e/XdkVPzMdDDsD8MM/Scream_Queens_S01E02.mkv" },
+                { episode: 3, video: "https://streamtape.com/e/9prKj0Wy6DH6p8/Scream_Queens_S01E03.mkv" },
+                { episode: 4, video: "https://streamtape.com/e/ogxoyb0my2UJzGa/Scream_Queens_S01E04.mkv" },
+                { episode: 5, video: "https://streamtape.com/e/AKYMVLJDVkT4Vw/Scream_Queens_S01E05.mkv" },
+                { episode: 6, video: "https://streamtape.com/e/WZMMYvGJPmIe4P/Scream_Queens_S01E06.mkv" },
+                { episode: 7, video: "https://streamtape.com/e/r0LAXXZMYWH3Dz/Scream_Queens_S01E07.mkv" },
+                { episode: 8, video: "https://streamtape.com/e/v9yleGvxjbU4qLP/Scream_Queens_S01E08.mkv" },
+                { episode: 9, video: "https://streamtape.com/e/Bjza1GoqJLtydgQ/Scream_Queens_S01E09.mkv" },
+                { episode: 10, video: "https://streamtape.com/e/V6RKma1gyXIK2rV/Scream_Queens_S01E10.mkv" },
+                { episode: 11, video: "https://streamtape.com/e/ApWayp6VOyUXo8e/Scream_Queens_S01E11.mkv" },
+                { episode: 12, video: "https://streamtape.com/e/W8G948k6OPUA0L/Scream_Queens_S01E12.mkv" },
+                { episode: 13, video: "https://streamtape.com/e/yPWx222koKU1LLa/Scream_Queens_S01E13.mkv" }
+            ]
+        },
+        {
+            season: 2,
+            episodes: [
+                { episode: 1, video: "https://streamtape.com/e/O1964bRBe4HZpLk/Scream_Queens_S02E01.mkv" },
+                { episode: 2, video: "https://streamtape.com/e/jkw603b081FzkjV/Scream_Queens_S02E02.mkv" },
+                { episode: 3, video: "https://streamtape.com/e/bgeaRpOGeRIPXxJ/Scream_Queens_S02E03.mkv" },
+                { episode: 4, video: "https://streamtape.com/e/oxje707Q4BtJ8g4/Scream_Queens_S02E04.mkv" },
+                { episode: 5, video: "https://streamtape.com/e/wgR8e4j0aQUl0J/Scream_Queens_S02E05.mkv" },
+                { episode: 6, video: "https://streamtape.com/e/Q1JGpVlDjMu0eej/Scream_Queens_S02E06.mkv" },
+                { episode: 7, video: "https://streamtape.com/e/ZKxB8RVkVMFqvdK/Scream_Queens_S02E07.mkv" },
+                { episode: 8, video: "https://streamtape.com/e/0L4WPV3rkaibGAg/Scream_Queens_S02E08.mkv" },
+                { episode: 9, video: "https://streamtape.com/e/oeYJjby2vGhJlQr/Scream_Queens_S02E09.mkv" },
+                { episode: 10, video: "https://streamtape.com/e/aGR4zR73Mmhxjwy/Scream_Queens_S02E10.mkv" }
+            ]
+        }
+    ]
+});
