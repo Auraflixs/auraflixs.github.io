27164f56d8c65b286a313d6bd6eaf4dcab542232
Auraflix <lestheradonys@gmail.com>
2026-04-11 13:29:42 -0600

    Update peli82.js

--- p/peli82.js
+++ p/peli82.js
@@ -8,7 +8,7 @@ window.moviesList.push({
     tags: ["Project Hail Mary", "Proyecto Fin del mundo"],
     image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/7lwOTxajURKEWO6gI370NTrVdBO.webp",
     trailer: "https://www.youtube.com/embed/in-lUuKi0eE",
-    video: "https://streamtape.com/e/oe6Kr9xlqaIJzgr/Proyecto_Fin_del_mundo_-_1.mp4",
+    video: "https://streamtape.com/e/O4rLGOebQ1tZVYj/Devoradores_de_Estrelas_-_1.mp4",
     info: "Ryland Grace es el único superviviente en una misión desesperada por salvar la humanidad y la Tierra. Sin recuerdos de quién es ni de su tarea, deberá usar la ciencia y a un inesperado aliado para evitar la extinción de ambos mundos.",
     featured: true
 });
