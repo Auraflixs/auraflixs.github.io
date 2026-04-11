d1ec736fd7168c0053e42d7256d5514f4a8dca0b
Auraflix <lestheradonys@gmail.com>
2026-04-10 21:21:52 -0600

    Add files via upload

---
+++ p/peli91.js
@@ -0,0 +1,14 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p91",
+    title: "Lindas y Letales",
+    year: "2026",
+    genre: "Acción, Suspenso, Crimen",
+    tags: ["Pretty Lethal", "Lindas y letales", "Ballerinas"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/cmZqxJQ9MdNQEoD49HYghOXsiwH.webp",
+    trailer: "https://www.youtube.com/embed/MpNobYCw0mg",
+    video: "https://streamtape.com/e/r9w717zrmaTbVwa/Pretty_Lethal_1080p_DUAL_id_1084187.mkv",
+    info: "Un grupo de talentosas bailarinas de ballet que viajan a una importante exhibición en Budapest termina atrapado en una pesadilla cuando son secuestradas por una red criminal. Lo que sus captores no saben es que años de disciplina, dolor y precisión las han convertido en guerreras naturales. Ahora, deberán usar su fuerza y agilidad para luchar por su libertad y sobrevivir.",
+    featured: true
+});
