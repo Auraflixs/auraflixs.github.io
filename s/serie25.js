45bc0254a57c09f8cb40fa9640c6cb051830464a
Auraflix <lestheradonys@gmail.com>
2026-04-10 21:22:10 -0600

    Add files via upload

---
+++ s/serie25.js
@@ -0,0 +1,30 @@
+window.seriesList = window.seriesList || [];
+
+window.seriesList.push({
+    id: "s25",
+    title: "Spartacus: La Casa de Ashur",
+    year: "2025",
+    genre: "Acción, Drama, Historia",
+    tags: ["Spartacus", "House of Ashur", "Gladiadores"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/bV50amPMtGHJuosHgQylN43oWaD.webp",
+    trailer: "https://www.youtube.com/embed/09l-Vwha48c",
+    info: "Esta serie presenta una historia alternativa donde Ashur sobrevive y recibe su propio ludus como recompensa. La trama sigue su ascenso al poder mientras entrena a una nueva generación de gladiadores, desafiando las normas de la arena romana.",
+    featured: true,
+    seasons: [
+        {
+            season: 1,
+            episodes: [
+                { episode: 1, video: "https://streamtape.com/e/jVdbkYPrGZSz0ka/Spartacus_House_Of_Ashur_720p_S01E01.mkv" },
+                { episode: 2, video: "https://streamtape.com/e/jZqXbkZAKGhzVr7/Spartacus_House_Of_Ashur_720p_S01E02.mkv" },
+                { episode: 3, video: "https://streamtape.com/e/RqLBdVgJklsx67/Spartacus_House_Of_Ashur_720p_S01E03.mkv.mp4" },
+                { episode: 4, video: "https://streamtape.com/e/v9KLqvRvXyf4vZl/Spartacus_House_Of_Ashur_720p_S01E04.mkv" },
+                { episode: 5, video: "https://streamtape.com/e/Laz6Y7Yj2aHRloo/Spartacus_House_Of_Ashur_720p_S01E05.mkv" },
+                { episode: 6, video: "https://streamtape.com/e/0zZKXW1B8lUb4xk/Spartacus_House_Of_Ashur_720p_S01E06.mkv" },
+                { episode: 7, video: "https://streamtape.com/e/PZ2bjzpjz2h0ZLQ/Spartacus_House_Of_Ashur_720p_S01E07.mkv" },
+                { episode: 8, video: "https://streamtape.com/e/oARYLl9GXaUJaZL/Spartacus_House_Of_Ashur_720p_S01E08.mkv" },
+                { episode: 9, video: "https://streamtape.com/e/KAPPeKzzo1IDvJ/Spartacus_House_of_Ashur_1080p_S01E09.mkv" },
+                { episode: 10, video: "https://streamtape.com/e/YyaD7RjRGeHejw/Spartacus_House_Of_Ashur_720p_S01E10.mkv" }
+            ]
+        }
+    ]
+});
---
+++ s/serie26.js
@@ -0,0 +1,38 @@
+window.seriesList = window.seriesList || [];
+
+window.seriesList.push({
+    id: "s26",
+    title: "Daredevil: Born Again",
+    year: "2025",
+    genre: "Acción, Suspenso, Crimen",
+    tags: ["Daredevil", "Born Again", "Matt Murdock", "Kingpin"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/vIwD2Wlu43EqcOBV1XSio0SZ7bI.webp",
+    trailer: "https://www.youtube.com/embed/qB9UFWn3iIU",
+    info: "Matt Murdock y Wilson Fisk intentan dejar atrás sus identidades de Daredevil y Kingpin para servir a la gente de Nueva York, pero sus pasados resurgen inevitablemente. En un entorno político y criminal complejo, ambos deberán enfrentarse a la realidad de quiénes son realmente mientras luchan por el alma de la ciudad.",
+    featured: true,
+    seasons: [
+        {
+            season: 1,
+            episodes: [
+                { episode: 1, video: "https://streamtape.com/e/dOPL3GWVmWsk66X/Daredevil_Born_Again_S01E01.mkv" },
+                { episode: 2, video: "https://streamtape.com/e/3oexYw3kMDIdYPQ/Daredevil_Born_Again_S01E02.mkv" },
+                { episode: 3, video: "https://streamtape.com/e/e01Ae0zlkdIkyZ/Daredevil_Born_Again_S01E03.mkv" },
+                { episode: 4, video: "https://streamtape.com/e/WZVwqwbkx8IblmY/Daredevil_Born_Again_S01E04.mkv" },
+                { episode: 5, video: "https://streamtape.com/e/M09xqzyvXDFelL/Daredevil_Born_Again_S01E05.mkv" },
+                { episode: 6, video: "https://streamtape.com/e/7pQL4yKBQviArxl/Daredevil_Born_Again_S01E06.mkv" },
+                { episode: 7, video: "https://streamtape.com/e/yBKjL0xvPQF1D9L/Daredevil_Born_Again_S01E07.mkv" },
+                { episode: 8, video: "https://streamtape.com/e/RyZ1ej3lgzSd3P4/Daredevil_Born_Again_S01E08.mkv" },
+                { episode: 9, video: "https://streamtape.com/e/jvrGwrVX06cmQ0/Daredevil_Born_Again_S01E09.mkv" }
+            ]
+        },
+        {
+            season: 2,
+            episodes: [
+                { episode: 1, video: "https://streamtape.com/e/jgd3Mr99bwfzKkW/Daredevil_Born_Again_S02E01.mkv" },
+                { episode: 2, video: "https://streamtape.com/e/DAly76lMM3fwML/Daredevil_Born_Again_S02E02.mkv" },
+                { episode: 3, video: "https://streamtape.com/e/jbyBl0YkvWszZBk/Daredevil_Born_Again_S02E03.mkv" },
+                { episode: 4, video: "https://streamtape.com/e/YgDO9AXoA0svv0J/Daredevil_Born_Again_S02E04.mkv.mp4" }
+            ]
+        }
+    ]
+});
