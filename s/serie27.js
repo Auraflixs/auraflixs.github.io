6f5ff201046ffc6ffc5b489f646fc5349ceede21
Auraflix <lestheradonys@gmail.com>
2026-04-12 11:23:30 -0600

    Add files via upload

---
+++ s/serie27.js
@@ -0,0 +1,41 @@
+window.seriesList = window.seriesList || [];
+
+window.seriesList.push({
+    id: "s27",
+    title: "One Piece",
+    year: "2023",
+    genre: "Acción, Aventura, Fantasía",
+    tags: ["Luffy", "Piratas"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/lN9588bZILlwSfHpjNC3uY6eauA.webp",
+    trailer: "https://www.youtube.com/embed/gV30eMvRZr8",
+    info: "Acompaña a Monkey D. Luffy y su joven tripulación de los Piratas de Sombrero de Paja en una épica aventura por la Grand Line. En su búsqueda del legendario tesoro 'One Piece' para convertirse en el Rey de los Piratas, deberán enfrentar peligrosos enemigos, forjar alianzas inesperadas y superar los desafíos del mar.",
+    featured: true,
+    seasons: [
+        {
+            season: 1,
+            episodes: [
+                { episode: 1, video: "https://streamtape.com/e/qOY88lAqXQhzqMj/One_Piece_S01E01.mkv.mp4" },
+                { episode: 2, video: "https://streamtape.com/e/eYz4rxmMQbcYPr2/One_Piece_S01E02.mkv.mp4" },
+                { episode: 3, video: "https://streamtape.com/e/jap34G2VxvszOy7/One_Piece_S01E03.mkv.mp4" },
+                { episode: 4, video: "https://streamtape.com/e/2bvp8qaR2zfZvW9/One_Piece_S01E04.mkv.mp4" },
+                { episode: 5, video: "https://streamtape.com/e/AXkBQwYZ3xIWXX/One_Piece_S01E05.mkv.mp4" },
+                { episode: 6, video: "https://streamtape.com/e/gvBJvOG1GpHqjYA/One_Piece_S01E06.mkv.mp4" },
+                { episode: 7, video: "https://streamtape.com/e/G2ezRL4rbjSdg7/One_Piece_S01E07.mkv.mp4" },
+                { episode: 8, video: "https://streamtape.com/e/q6WYBmgRlJCzelq/One_Piece_S01E08.mkv.mp4" }
+            ]
+        },
+        {
+            season: 2,
+            episodes: [
+                { episode: 1, video: "https://streamtape.com/e/mvMZ36946MC6aL/One_Piece_S02E01.mkv" },
+                { episode: 2, video: "https://streamtape.com/e/VymgaLmmK3TKROK/One_Piece_S02E02.mkv" },
+                { episode: 3, video: "https://streamtape.com/e/dVGeeQYO46I0mK/One_Piece_S02E03.mkv" },
+                { episode: 4, video: "https://streamtape.com/e/3Od9Lr4ZekuXMR/One_Piece_S02E04.mkv" },
+                { episode: 5, video: "https://streamtape.com/e/qaq3Dj8w2aHzdyk/One_Piece_S02E05.mkv" },
+                { episode: 6, video: "https://streamtape.com/e/Zbq1byK82VuqV9w/One_Piece_S02E06.mkv" },
+                { episode: 7, video: "https://streamtape.com/e/Bb317AeooLt6DO/One_Piece_S02E07.mkv" },
+                { episode: 8, video: "https://streamtape.com/e/LqYg3aovZycRlwd/One_Piece_S02E08.mkv" }
+            ]
+        }
+    ]
+});
