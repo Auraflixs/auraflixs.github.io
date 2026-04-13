70981c70ca52b41b0ea8bb51aed09ef66548544c
Auraflix <lestheradonys@gmail.com>
2026-04-13 14:57:23 -0600

    Update peli73.js

--- p/peli73.js
+++ p/peli73.js
@@ -6,7 +6,7 @@ window.moviesList.push({
     tags: ["The Strangers: Chapter 3"],
     image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/yPegTig8dQNYiW2UW21LTNChGkK.webp",
     trailer: "https://www.youtube.com/embed/B2fZEIHamVs",
-    video: "https://streamtape.com/e/xv3MlVqKKqCkApD/Los_Extra%C3%B1os_Cap%C3%ADtulo_3_-_Parte_3_-_1.mp4",
+    video: "https://streamtape.com/e/vL78w8XPQls4mm6/Los_Extra%C3%B1os_Cap%C3%ADtulo_3_1080p_DUAL_id_1010755.mkv",
     year: 2026,
     genres: ["Terror", "Suspenso"],
     info: "En el capítulo final de esta trilogía de terror, se revelan los oscuros motivos detrás de los enmascarados mientras la última superviviente se ve obligada a enfrentarse a su pasado para poner fin a la pesadilla. Un cierre brutal que expande el universo de 'Los Extraños' y responde a la pregunta definitiva: ¿Por qué ellos?",
