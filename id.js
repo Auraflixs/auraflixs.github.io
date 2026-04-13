cf1abdb5cb3a66fd3b71816fda382da47b93db78
Auraflix <lestheradonys@gmail.com>
2026-04-12 21:15:38 -0600

    Update id.js

--- id.js
+++ id.js
@@ -1,32 +1,28 @@
 window.HERO_IDS = [
    {
-   id:"s5",
-   img:"https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/hero/IMG-20260412-WA0104.webp",
+      id: "s5",
+      img: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/hero/IMG-20260412-WA0104.webp",
    },
    {
-   id:"s5_alt", // 👈 duplicado para "últimamente"
-   img:"https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/vFur1VinsqHGvI3UBZq3sjo6YP8.webp",
+      id: "p83",
+      img: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/hero/dv4z4BNuaZWUfzNxDfLDD542vCi.webp",
    },
    {
-   id:"p83",
-   img:"https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/hero/dv4z4BNuaZWUfzNxDfLDD542vCi.webp",
+      id: "p82",
+      img: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/hero/cdK9EBDzRvRg7oYLAUNyx6Cqgi1.webp",
    },
    {
-   id:"p82",
-   img:"https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/hero/cdK9EBDzRvRg7oYLAUNyx6Cqgi1.webp",
+      id: "p79",
+      img: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/hero/ytmQOsyYNypGDJCkTJ2TPP3Jc0u.jpg",
    },
    {
-   id:"p79",
-   img:"https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/hero/ytmQOsyYNypGDJCkTJ2TPP3Jc0u.jpg",
-   },
-   {
-   id:"s23",
-   img:"https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/hero/A0XppRn02zx5obIiLjHZPr8EZgW.jpg",
+      id: "s23",
+      img: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/hero/A0XppRn02zx5obIiLjHZPr8EZgW.jpg",
    }
 ];
 
 window.LATELY_IDS = [
-    "s5_alt", // 👈 aquí usa la imagen nueva 😈
+    { id: "s5", img: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/vFur1VinsqHGvI3UBZq3sjo6YP8.webp" }, // override solo para "Últimamente"
     "p83",
     "p82",
     "p84",
