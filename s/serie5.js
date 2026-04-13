4ab05cc2a2d6b64d66d5393b6a1a326d76d12b5c
Auraflix <lestheradonys@gmail.com>
2026-04-12 20:46:10 -0600

    Add files via upload

---
+++ s/serie5.js
@@ -0,0 +1,49 @@
+window.seriesList = window.seriesList || [];
+
+window.seriesList.push({
+    id: "s5",
+    title: "Euphoria",
+    year: "2019",
+    genres: ["Drama"],
+    image: "https://raw.githubusercontent.com/Auraflixs/im-genestv/refs/heads/main/9MmQrUGUbDDl2lHMwZvhjgNnkZt.webp",
+    trailer: "https://www.youtube.com/embed/P7UDF9LHo48?start=6",
+    info: "Un grupo de estudiantes de secundaria navega por las turbulentas aguas del amor y la amistad en un mundo dominado por las redes sociales, el trauma y las adicciones. La serie sigue de cerca a Rue Bennett, una joven de 17 años en rehabilitación que lucha por encontrar su lugar y la esperanza mientras lidia con la pérdida, la identidad y las complejidades de la vida moderna.",
+    featured: true,
+    seasons: [
+        {
+            season: 1,
+            episodes: [
+                { episode: 1, video: "https://streamtape.com/e/jlpqX2rbjXszw8P/Euphorian_S01E01.mkv.mp4", unlockLink: "https://direct-link.net/2492660/wZvLLDtnG0Jn" },
+                { episode: 2, video: "https://streamtape.com/e/jq2zArPdkdHz1wp/Euphorian_S01E02.mkv.mp4", unlockLink: "https://direct-link.net/2492660/tiN1wGU2NJ9X" },
+                { episode: 3, video: "https://streamtape.com/e/R6rr1rOK6DSd3bR/Euphorian_S01E03.mkv.mp4", unlockLink: "https://direct-link.net/2492660/n9xWkwsCGmpC" },
+                { episode: 4, video: "https://streamtape.com/e/jYo248mrbXczj9m/Euphorian_S01E04.mkv.mp4", unlockLink: "https://link-hub.net/2492660/bsYxjavZbfhm" },
+                { episode: 5, video: "https://streamtape.com/e/VyPR7owX1Qtyw4/Euphorian_S01E05.mkv.mp4", unlockLink: "https://link-hub.net/2492660/YTNdO5nUjF23" },
+                { episode: 6, video: "https://streamtape.com/e/yDRB4kRVZ9S18Y3/Euphorian_S01E06.mkv.mp4", unlockLink: "https://link-center.net/2492660/7H6DFAwTN53z" },
+                { episode: 7, video: "https://streamtape.com/e/WoLR7W8M7xubVvX/Euphorian_S01E07.mkv.mp4", unlockLink: "https://link-target.net/2492660/pbJycBCR8Xxy" },
+                { episode: 8, video: "https://streamtape.com/e/vB74llqVqaUY3m/Euphorian_S01E08.mkv.mp4", unlockLink: "https://link-hub.net/2492660/nS5vqyRMiAj8" }
+            ]
+        },
+        {
+            season: 2,
+            episodes: [
+                { episode: 1, video: "https://streamtape.com/e/zbG7yoz8z8IYw1D/Euphoria_S02E01.mkv.mp4", unlockLink: "https://direct-link.net/2492660/rlh6gaOSc2Ei" },
+                { episode: 2, video: "https://streamtape.com/e/Kq2W6ZQjQBf007G/Euphoria_S02E02.mkv.mp4", unlockLink: "https://link-hub.net/2492660/TZj9nOAH4ZKD" },
+                { episode: 3, video: "https://streamtape.com/e/q8RP1Od340IzqpO/Euphoria_S02E03.mkv.mp4", unlockLink: "https://link-target.net/2492660/tTlAHYqwZeeY" },
+                { episode: 4, video: "https://streamtape.com/e/De9Pb9zXOkCkozy/Euphoria_S02E04.mkv.mp4", unlockLink: "https://link-hub.net/2492660/Cuu1fdLYImag" },
+                { episode: 5, video: "https://streamtape.com/e/9R4v3YBw7aTalmo/Euphoria_S02E05.mkv.mp4", unlockLink: "https://link-center.net/2492660/SxBrRxQUJYcx" },
+                { episode: 6, video: "https://streamtape.com/e/w7ZGLMgmWmUJbqy/Euphoria_S02E06.mkv.mp4", unlockLink: "https://link-target.net/2492660/aPAVmDW6xRcz" },
+                { episode: 7, video: "https://streamtape.com/e/7DgqQ3gZVpiALa8/Euphoria_S02E07.mkv.mp4", unlockLink: "https://link-center.net/2492660/Tujn6RQfMUtu" },
+                { episode: 8, video: "https://streamtape.com/e/VXbAaYXBv8sKVWK/Euphoria_S02E08.mkv.mp4", unlockLink: "https://link-hub.net/2492660/qgapHS1A2MQ3" }
+            ]
+        },
+        {
+            season: 3,
+            episodes: [
+                { 
+                    episode: 1, 
+                    video: "https://streamtape.com/e/8OOxZ9l4oPUj1o/Euphoria_S3E1.mp4", 
+                }
+            ]
+        }
+    ]
+});
