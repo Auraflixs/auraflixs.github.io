991fa2038877ba7a46f3a8fe4ad4410594afc40a
Auraflix <lestheradonys@gmail.com>
2026-04-14 08:50:31 -0600

    Add files via upload

---
+++ s/serie28.js
@@ -0,0 +1,24 @@
+window.seriesList = window.seriesList || [];
+
+window.seriesList.push({
+    id: "s28",
+    title: "Malcolm: de mal en peor",
+    year: "2026",
+    genre: "Comedia, Drama",
+    tags: ["Malcolm", "Life's Still Unfair", "Bryan Cranston", "Frankie Muniz"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/bqYCU8Xv8CCfJm4HfznGzfgRJOp.webp",
+    trailer: "https://www.youtube.com/embed/ABol0H2n_rc",
+    info: "Años después de los eventos de la serie original, la familia se reúne para un evento especial de cuatro partes. Malcolm intenta equilibrar su vida adulta con las locuras persistentes de sus hermanos y unos padres que no han cambiado mucho. Un regreso nostálgico que demuestra que, sin importar cuánto tiempo pase, la vida sigue siendo injusta.",
+    featured: true,
+    seasons: [
+        {
+            season: 1,
+            episodes: [
+                { episode: 1, video: "https://streamtape.com/e/4PzberrzQZfKerx/Malcolm_In_The_Middle_La_Vida_Sigue_Siendo_Injusta_S01E01.mkv" },
+                { episode: 2, video: "https://streamtape.com/e/3Omk6rlXZkUdywM/Malcolm_In_The_Middle_La_Vida_Sigue_Siendo_Injusta_S01E02.mkv" },
+                { episode: 3, video: "https://streamtape.com/e/RL1DXLY2RJidAGx/Malcolm_In_The_Middle_La_Vida_Sigue_Siendo_Injusta_S01E03.mkv" },
+                { episode: 4, video: "https://streamtape.com/e/OXkWMkDLgbsZRlr/Malcolm_In_The_Middle_La_Vida_Sigue_Siendo_Injusta_S01E04.mkv" }
+            ]
+        }
+    ]
+});
