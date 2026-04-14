7237c53bf488ec3629c0cfd9887ea6c6bbe0ae4e
Auraflix <lestheradonys@gmail.com>
2026-04-13 20:20:25 -0600

    Add files via upload

---
+++ p/peli96.js
@@ -0,0 +1,14 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p96",
+    title: "Una película de Minecraft",
+    year: "2025",
+    genre: "Aventura, Fantasía, Acción",
+    tags: ["Minecraft", "Steve", "Jack Black", "Jason Momoa"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/wTxYZNj3NselE9H9dST2GUn4UYE.webp",
+    trailer: "https://www.youtube.com/embed/NOAmJa-dhCA",
+    video: "https://streamtape.com/e/7pk4mQKGXGFAAAk/Una_Pel%C3%ADcula_de_Minecraft_1080p_DUAL_id_950387.mkv",
+    info: "Cuatro inadaptados se ven arrastrados a través de un portal misterioso hacia el Overworld: un extraño y cúbico mundo que vive de la imaginación. Para volver a casa, tendrán que dominar este mundo y protegerlo de criaturas malvadas como los Piglins y los Zombies, mientras se embarcan en una búsqueda mágica junto a un experto constructor llamado Steve.",
+    featured: true
+});
