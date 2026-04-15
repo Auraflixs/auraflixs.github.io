7a96d8888f932953fe95b86c618823e162b6a3ec
Auraflix <lestheradonys@gmail.com>
2026-04-14 22:28:40 -0600

    Add files via upload

---
+++ p/peli103.js
@@ -0,0 +1,14 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p103",
+    title: "abracadabra 2",
+    year: "2022",
+    genre: "Fantasía, Comedia, Terror",
+    tags: ["Hocus Pocus", "El Retorno de las Brujas 2", "Sanderson Sisters", "Halloween"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/l4kYnOX3HPZzfjtkeDMl80iY126.webp",
+    trailer: "https://www.youtube.com/embed/YQKkDuTVRbE",
+    video: "https://streamtape.com/e/Aql9yr8oVytXlXk/Hocus_Pocus_2_1080p_DUAL_id_642885.mkv",
+    info: "Han pasado 29 años desde que alguien encendió la vela de llama negra y resucitó a las hermanas Sanderson del siglo XVII, y ahora buscan venganza. De tres estudiantes de secundaria depende impedir que las voraces brujas causen un nuevo tipo de caos en Salem antes de que amanezca en la víspera de Todos los Santos.",
+    featured: true
+});
