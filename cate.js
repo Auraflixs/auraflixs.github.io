1c72e0b8d146bbaaa2ef508a88e574f57bfd18c8
Auraflix <lestheradonys@gmail.com>
2026-04-15 16:52:02 -0600

    Add files via upload

---
+++ cate.js
@@ -0,0 +1,23 @@
+// Categorías que aparecerán en el Inicio (Acepta P y S mezclados)
+window.customCategories = [
+    {
+        title: "El Universo del Conjuro",
+        ids: ["p57", "p56", "p58", "p54", "p55", "p59", "p60", "p61", "p6"]
+    }
+];
+
+// Categorías que aparecerán SOLO en la pestaña Películas
+window.movieCategories = [
+    {
+        title: "Sagas Épicas",
+        ids: ["p10", "p11", "p12"]
+    }
+];
+
+// Categorías que aparecerán SOLO en la pestaña Series
+window.seriesCategories = [
+    {
+        title: "Series de Ciencia Ficción",
+        ids: ["s1", "s2", "s3"]
+    }
+];
