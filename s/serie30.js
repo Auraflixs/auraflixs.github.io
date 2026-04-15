4fc8d33df2bdbc1f3241e8fa2f30fe911c2917da
Auraflix <lestheradonys@gmail.com>
2026-04-14 21:17:11 -0600

    Add files via upload

---
+++ s/serie30.js
@@ -0,0 +1,88 @@
+window.seriesList = window.seriesList || [];
+
+window.seriesList.push({
+    id: "s30",
+    title: "La Casa de Papel",
+    year: "2017",
+    genre: "Acción, Suspenso, Crimen",
+    tags: ["Money Heist", "El Profesor", "Netflix", "Atraco"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/z01Dc0Ly2GmCpLe6Scx4d3dPP1S.webp",
+    trailer: "https://www.youtube.com/embed/95b51iWL1Uc",
+    info: "Un misterioso personaje, conocido como 'El Profesor', planea el mayor atraco de la historia: entrar en la Fábrica Nacional de Moneda y Timbre para imprimir 2.400 millones de euros. Para llevar a cabo el plan, recluta a una banda de ocho personas con habilidades especiales que no tienen nada que perder. La resistencia ha comenzado.",
+    featured: true,
+    seasons: [
+        {
+            season: 1,
+            episodes: [
+                { episode: 1, video: "https://streamtape.com/e/KyG0yrKW9yC0Ver/" },
+                { episode: 2, video: "https://streamtape.com/e/qWbA0wp43DhYjQ/" },
+                { episode: 3, video: "https://streamtape.com/e/PXdALz9o13U0wo4/" },
+                { episode: 4, video: "https://streamtape.com/e/pD8exOrRKDuRQQ/" },
+                { episode: 5, video: "https://streamtape.com/e/JA0zoeQwWrhjY1R/" },
+                { episode: 6, video: "https://streamtape.com/e/A3Qpjb0KXViXMVX/" },
+                { episode: 7, video: "https://streamtape.com/e/D2gR76bkg4tkKlr/" },
+                { episode: 8, video: "https://streamtape.com/e/PwxgXapVAoS0w9W/" },
+                { episode: 9, video: "https://streamtape.com/e/ZzZDaRkMGyHPV2/" },
+                { episode: 10, video: "https://streamtape.com/e/wzlKGdpKgXSJ221/" },
+                { episode: 11, video: "https://streamtape.com/e/YevykMXVlGFrB8/" },
+                { episode: 12, video: "https://streamtape.com/e/l230MA81QZu71Wy/" },
+                { episode: 13, video: "https://streamtape.com/e/Q1pAyVg2r9S0QZX/" }
+            ]
+        },
+        {
+            season: 2,
+            episodes: [
+                { episode: 1, video: "https://streamtape.com/e/ybxyWdo19kC1612/" },
+                { episode: 2, video: "https://streamtape.com/e/B7y20q79KbHyXKq/" },
+                { episode: 3, video: "https://streamtape.com/e/waVpXq7r26cJ1qA/" },
+                { episode: 4, video: "https://streamtape.com/e/P89xgMydryF02kk/" },
+                { episode: 5, video: "https://streamtape.com/e/xb8vVM0mgvHkPvz/" },
+                { episode: 6, video: "https://streamtape.com/e/9RzAgpXxvXFoxy/" },
+                { episode: 7, video: "https://streamtape.com/e/jWQkKGjoo6UzZl8/" },
+                { episode: 8, video: "https://streamtape.com/e/XwDlV0GmwmFAV6/" },
+                { episode: 9, video: "https://streamtape.com/e/09m4k3OPOAhb8LJ/" }
+            ]
+        },
+        {
+            season: 3,
+            episodes: [
+                { episode: 1, video: "https://streamtape.com/e/DG7yO1DVRgTkvze/" },
+                { episode: 2, video: "https://streamtape.com/e/WXDdayRAjPtbbrd/" },
+                { episode: 3, video: "https://streamtape.com/e/MJ2Go28OBXcmkgm/" },
+                { episode: 4, video: "https://streamtape.com/e/JbWLYRJZlAFjXok/" },
+                { episode: 5, video: "https://streamtape.com/e/3Jx0lagWRXtXdP/" },
+                { episode: 6, video: "https://streamtape.com/e/Rg461Mdj3wH0Rd/" },
+                { episode: 7, video: "https://streamtape.com/e/6wzjqyRPBOulWg/" },
+                { episode: 8, video: "https://streamtape.com/e/167wAQlwwpCe23K/" }
+            ]
+        },
+        {
+            season: 4,
+            episodes: [
+                { episode: 1, video: "https://streamtape.com/e/BkGKPLK64QsWvg/" },
+                { episode: 2, video: "https://streamtape.com/e/3w27RZv14ou30k/" },
+                { episode: 3, video: "https://streamtape.com/e/B4OQbW76Vqty0gO/" },
+                { episode: 4, video: "https://streamtape.com/e/aPkkQmo8gVuxrxZ/" },
+                { episode: 5, video: "https://streamtape.com/e/pd8dblDAVxTAd4/" },
+                { episode: 6, video: "https://streamtape.com/e/laADamJ0vpHvvg/" },
+                { episode: 7, video: "https://streamtape.com/e/m1ZzJDrXlBcb8P9/" },
+                { episode: 8, video: "https://streamtape.com/e/08KdgedbWQSbXqy/" }
+            ]
+        },
+        {
+            season: 5,
+            episodes: [
+                { episode: 1, video: "https://streamtape.com/e/Mzx7Jbm8d4TmVyw/" },
+                { episode: 2, video: "https://streamtape.com/e/zG7r8RLXVvFY3k7/" },
+                { episode: 3, video: "https://streamtape.com/e/O4qoGvbxvVIGlg/" },
+                { episode: 4, video: "https://streamtape.com/e/A0AQ0Rr6oXSwRr/" },
+                { episode: 5, video: "https://streamtape.com/e/4oKvPQ2O7YIKoZo/" },
+                { episode: 6, video: "https://streamtape.com/e/RmKYlw3wv2TdWXk/" },
+                { episode: 7, video: "https://streamtape.com/e/Oapq3WZ8M4SZrm4/" },
+                { episode: 8, video: "https://streamtape.com/e/j73bj8wde8Hzeob/" },
+                { episode: 9, video: "https://streamtape.com/e/D9DwJg2Zb0skJ4Z/" },
+                { episode: 10, video: "https://streamtape.com/e/3AOXem0RgQFakb/" }
+            ]
+        }
+    ]
+});
