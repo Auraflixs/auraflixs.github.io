72893876856bd55bca9f4ca796358dc42863fbdd
Auraflix <lestheradonys@gmail.com>
2026-04-15 20:40:46 -0600

    Add files via upload

---
+++ p/peli106.js
@@ -0,0 +1,15 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p106",
+    globalid: "2",
+    title: "El Exorcismo De Dios",
+    year: "2022",
+    genre: "Terror, Drama", "Crimen", "Fantasía",
+    tags: ["The Exorcism of God"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/aSfvsaIdtF5eXf8IsvIYdzhI3XL%20(1).webp",
+    trailer: "https://www.youtube.com/embed/JAMdGGDxkIQ",
+    video: "https://streamtape.com/e/GXjQk203ZWu1eXR/El_exorcismo_deDios_1080p_LAT_id_836225.mp4",
+    info: "Un sacerdote estadounidense que trabaja en México es poseído durante un exorcismo y termina cometiendo un terrible pecado. Dieciocho años después, las consecuencias de ese error regresan para atormentarlo, desatando una batalla épica contra el mal.",
+    featured: true
+});
