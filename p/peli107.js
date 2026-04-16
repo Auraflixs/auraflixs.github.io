71b9e958d0000781a357398395100e4c53ab821b
Auraflix <lestheradonys@gmail.com>
2026-04-15 20:48:32 -0600

    Add files via upload

---
+++ p/peli107.js
@@ -0,0 +1,15 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p107",
+    globalid: "3",
+    title: "Ruega por nosotros",
+    year: "2021",
+    genre: "Terror", "Misterio",
+    tags: ["The Unholy", "La profecía"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/llHklmHQEDwTgbwGrPvQ5ouSy2k.webp",
+    trailer: "https://www.youtube.com/embed/NmQiJPLYzPI",
+    video: "https://streamtape.com/e/gVb4Y2P49VtJ8J/Ruega_Por_Nosotros_1080p_LAT_id_632357.mp4",
+    info: "Un periodista con poca suerte descubre una serie de milagros en un pequeño pueblo de Nueva Inglaterra atribuidos a la Virgen María. Sin embargo, pronto empiezan a ocurrir eventos aterradores que sugieren que el origen de estos fenómenos es mucho más siniestro.",
+    featured: true
+});
