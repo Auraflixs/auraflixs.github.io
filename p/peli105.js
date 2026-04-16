4785822d54161817b1fb5baa3815c05988bc7d02
Auraflix <lestheradonys@gmail.com>
2026-04-15 20:30:15 -0600

    Add files via upload

---
+++ p/peli105.js
@@ -0,0 +1,15 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p105",
+    globalid: "1",
+    title: "Outcome",
+    year: "2026",
+    genre: "Comedia, Drama",
+    tags: ["Outcome", "Resultado", "El Resultado"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/f3ggRkc3m4Dz8TH8Zt29HmD4fZZ.webp",
+    trailer: "https://www.youtube.com/embed/BGM_4AbrNlA",
+    video: "https://streamtape.com/e/z7q7o24gp2CYqjK/Outcome_1080p_DUAL_id_1049471",
+    info: "Rif Hawk, una gran estrella de cine, se ve envuelto en un misterioso video que amenaza su carrera, obligándolo a enfrentar su pasado y su gabinete de crisis.",
+    featured: true
+});
