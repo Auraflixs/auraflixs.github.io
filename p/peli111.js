a7b54a50303b57c9b18b7658fa2c996ac47e98ff
Auraflix <lestheradonys@gmail.com>
2026-04-17 01:22:49 -0600

    Add files via upload

---
+++ p/peli111.js
@@ -0,0 +1,15 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p111",
+    globalid: "7",
+    title: "Dolly",
+    year: "2026",
+    genre: "Terror, Suspenso",
+    tags: ["Dolly slasher"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/sTPUg3XtdcKRAS5Vwsi1GYPFhNZ.webp",
+    trailer: "https://www.youtube.com/embed/LALT2B-Ubpk",
+    video: "https://streamtape.com/e/kbO3KY7DxMTOoBy/Backwood.Madness.2025.1080p.WEBRip.LAT.DUB.1XBET.mp4",
+    info: "Un slasher aterrador donde una pareja que explora el bosque se topa con un proyecto artístico inquietante compuesto por muñecas. Pronto descubrirán que no están solos y que han entrado en el territorio de una asesina implacable que los obligará a participar en un juego macabro.",
+    featured: true
+});
