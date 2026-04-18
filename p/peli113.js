5f3ea15fa81e8685a4d7880c7bdde2869952f38f
Auraflix <lestheradonys@gmail.com>
2026-04-17 19:38:23 -0600

    Add files via upload

---
+++ p/peli113.js
@@ -0,0 +1,15 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p113",
+    globalid: "11",
+    title: "Exterminio: La evolución",
+    year: "2025",
+    genre: "Terror, Acción, Ciencia ficción",
+    tags: ["28 Years Later", "28 años después"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/gc5oHGz2AugoAlnfsmie1I0ZdOM.webp",
+    trailer: "https://www.youtube.com/embed/d-4F7jJUoOE",
+    video: "https://streamtape.com/e/voeGjBxDJrs4d0J/Exterminio_La_Evoluci%C3%B3n_DUAL_1080p_id_1100988.mkv",
+    info: "Casi tres décadas después de que el virus de la rabia asolara el Reino Unido, una nueva generación lucha por sobrevivir en un mundo post-apocalíptico donde la amenaza ha evolucionado de formas inesperadas.",
+    featured: true
+});
