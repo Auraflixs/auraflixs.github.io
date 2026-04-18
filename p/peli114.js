85a2f5c0a76ecaf2f82c0ac52099a4c2b23a0522
Auraflix <lestheradonys@gmail.com>
2026-04-17 23:29:26 -0600

    Update peli114.js

--- p/peli114.js
+++ p/peli114.js
@@ -9,7 +9,7 @@ window.moviesList.push({
     tags: ["Cruella de Vil"],
     image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/hjS9mH8KvRiGHgjk6VUZH7OT0Ng.webp",
     trailer: "https://www.youtube.com/embed/1DMmsFz6hj8",
-    video: "https://auraflix.seekplays.com/#xt9o6",
+    video: "https://auraflix.seekplays.com/#l9pzr",
     info: "Decidida a convertirse en una exitosa diseñadora de moda, una joven estafadora llamada Estella se hace amiga de un par de ladrones que aprecian su apetito por la travesura. Un día, el estilo de Estella llama la atención de la baronesa von Hellman, una leyenda de la moda, pero su relación pone en marcha un curso de acontecimientos que harán que Estella abrace su lado malvado.",
     featured: true
 });
