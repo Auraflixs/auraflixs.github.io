42df3154a9e4145a32ae9d2241a660bdca22926a
Auraflix <lestheradonys@gmail.com>
2026-04-17 19:44:35 -0600

    Update peli69.js

--- p/peli69.js
+++ p/peli69.js
@@ -6,7 +6,7 @@ window.moviesList.push({
     tags: ["Return to Silent Hill"],
     image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/fqAGFN2K2kDL0EHxvJaXzaMUkkt.webp",
     trailer: "https://www.youtube.com/embed/WHz68ffYJ0s",
-    video: "https://streamtape.com/e/k3xOQyvJKLu6Xm/Terror_En_Silent_Hill_Regreso_Al_Infierno_-_Parte_3_-_1.mp4",
+    video: "https://streamtape.com/e/D9ejJ87zyXHkjlY/Terror_en_Silent_Hill_Regreso_al_infierno_1080p_DUAL_id_680493.mkv",
     year: 2026,
     genres: ["Terror", "Misterio"],
     info: "Impulsado por las sombras de su pasado, James Sunderland regresa a Silent Hill para encontrar a su amor perdido, Mary Crane. Pero el pequeño pueblo ya no es como lo recordaba; se encuentra con figuras aterradoras tanto conocidas como nuevas, y comienza a cuestionar su propia cordura mientras intenta desentrañar el misterio de lo que le sucedió a su amada.",
