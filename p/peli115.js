7fcfeecc9f06d138a75d25a9e7285613362364b8
Auraflix <lestheradonys@gmail.com>
2026-04-19 10:54:15 -0600

    Add files via upload

---
+++ p/peli115.js
@@ -0,0 +1,15 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p115",
+    globalid: "13",
+    title: "The shadow's edge",
+    year: "2025",
+    genre: "Acción, Crimen",
+    tags: ["Operación Sombra", "Escuadrón letal", "El Filo de la Sombra"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/e0RU6KpdnrqFxDKlI3NOqN8nHL6.webp",
+    trailer: "https://www.youtube.com/embed/-XiwDPiIoZQ",
+    video: "https://auraflix.seekplays.com/#mdhxd",
+    info: "Una estrella de cine de acción y su representante se ven envueltos en una misión de rescate real cuando intentan salvar a un panda gigante de un grupo de mercenarios internacionales.",
+    featured: true
+});
