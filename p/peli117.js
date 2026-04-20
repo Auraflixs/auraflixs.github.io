310787ea8fdbf4314299de24a0c012b85288e7a5
Auraflix <lestheradonys@gmail.com>
2026-04-20 16:18:59 -0600

    Add files via upload

---
+++ p/peli117.js
@@ -0,0 +1,15 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p117",
+    globalid: "15",
+    title: "Compañeras de cuarto",
+    year: "2026",
+    genre: "Comedia, Drama",
+    tags: ["Roommates"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/kr0n41XFKd6chgZYyNtbD5gPleA.webp",
+    trailer: "https://www.youtube.com/embed/PF3dDdeSIVc",
+    video: "https://auraflix.seekplays.com/#sgqcx",
+    info: "El mayor reto para los estudiantes de primer año no es mudarse de casa o equilibrar la vida social, sino aprender a coexistir con un compañero de cuarto. Lo que comienza como una amistad inseparable pronto se pone a prueba con problemas de límites, drama universitario y situaciones inesperadas.",
+    featured: true
+});
