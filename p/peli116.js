6efc66954cff95ceb88c3498f9ba1190eeedd747
Auraflix <lestheradonys@gmail.com>
2026-04-19 23:46:09 -0600

    Add files via upload

---
+++ p/peli116.js
@@ -0,0 +1,15 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p116",
+    globalid: "14",
+    title: "Venganza",
+    year: "2026",
+    genre: "Acción, Crimen",
+    tags: ["Vengeance"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/ggJGx8fwwy21gEIXYusIlHcUn8z.webp",
+    trailer: "https://www.youtube.com/embed/w5ImAc3jMsE",
+    video: "https://auraflix.seekplays.com/#mdsp9",
+    info: "Un militar de élite busca justicia por su propia mano tras un violento ataque que cambia su vida para siempre. Con la ayuda de un aliado inesperado, se enfrentará a una red de corrupción y crimen para lograr su cometido.",
+    featured: true
+});
