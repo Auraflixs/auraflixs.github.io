58aad71ffee99ed7c8bab3a5771ad35606085d77
Auraflix <lestheradonys@gmail.com>
2026-04-21 09:35:26 -0600

    Add files via upload

---
+++ p/peli118.js
@@ -0,0 +1,15 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p118",
+    globalid: "16",
+    title: "La posesión de la momia",
+    year: "2026",
+    genre: "Terror",
+    tags: ["The Mummy"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/hGgSpTX2f3ShFD41Ul0vcY3DSHb.webp",
+    trailer: "https://www.youtube.com/embed/XJ0uv-phsDk",
+    video: "https://auraflix.seekplays.com/#codgl",
+    info: "Una nueva y aterradora visión del mito de la momia. Cuando una joven es encontrada viva tras haber estado desaparecida por ocho años dentro de un sarcófago antiguo, sus padres descubren que algo oscuro y ancestral ha regresado con ella.",
+    featured: true
+});
---
+++ p/peli119.js
@@ -0,0 +1,15 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p119",
+    globalid: "17",
+    title: "La leyenda de Aang: El último Airbender",
+    year: "2026",
+    genre: "Acción, Ciencia ficción",
+    tags: ["Avatar: The Last Airbender", "Aang: The Last Airbender", "El último maestro del aire"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/AbUa37L0mRs4RtMLvSD6cWZcPJP.webp",
+    trailer: "https://www.youtube.com/embed/KA_YIcMqsAs",
+    video: "https://auraflix.seekplays.com/#ku19f",
+    info: "La esperada continuación de la historia de Aang y sus amigos en formato de película animada, producida por Avatar Studios. En esta nueva aventura, los maestros de los elementos deberán enfrentar desafíos que pondrán a prueba el equilibrio del mundo una vez más.",
+    featured: true
+});
