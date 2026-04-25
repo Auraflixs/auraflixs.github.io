fb383c3496478319c320c2f53f56aeee9b9879ed
Auraflix <lestheradonys@gmail.com>
2026-04-25 16:26:37 -0600

    Add files via upload

---
+++ p/peli120.js
@@ -0,0 +1,15 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p120",
+    globalid: "19",
+    title: "Michael",
+    year: "2026",
+    genre: "Drama, Música",
+    tags: ["Michael Jackson"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/3Qud19bBUrrJAzy0Ilm8gRJlJXP.webp",
+    trailer: "https://www.youtube.com/embed/k-YAcjaLuSI",
+    video: "https://auraflix.seekplays.com/#hotne",
+    info: "Una crónica profunda de la vida y carrera de Michael Jackson, desde sus inicios con los Jackson 5 hasta convertirse en el 'Rey del Pop'. La película explora tanto su genio artístico como las complejidades de su vida personal y el inmenso impacto cultural que dejó en el mundo.",
+    featured: true
+});
---
+++ p/peli121.js
@@ -0,0 +1,15 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p121",
+    globalid: "20",
+    title: "El drama",
+    year: "2026",
+    genre: "Drama, Comedia, Romance",
+    tags: ["The Drama"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/hju7F2iZsasQ3kfVYxtO9OlbnvQ.webp",
+    trailer: "https://www.youtube.com/embed/0ZDzsH3XGFA",
+    video: "https://auraflix.seekplays.com/#hot1w",
+    info: "Una pareja que se prepara para dar el siguiente gran paso en su relación ve cómo todo da un vuelco inesperado cuando secretos del pasado comienzan a salir a la luz. Una historia que mezcla la tensión emocional con toques de humor y romance.",
+    featured: true
+});
