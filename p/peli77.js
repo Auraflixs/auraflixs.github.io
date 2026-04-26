a3bbb302f8e76e670fa1cf177f1fe1170b6f790f
Auraflix <lestheradonys@gmail.com>
2026-04-26 11:43:31 -0600

    Update peli77.js

--- p/peli77.js
+++ p/peli77.js
@@ -6,7 +6,7 @@ window.moviesList.push({
     tags: ["Whistle: El silbido del mal"],
     image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/cApRXj6meulAQMYQrhjfVvZEItl.webp",
     trailer: "https://www.youtube.com/embed/-O5DgGQJeks",
-    video: "https://auraflix.seekplays.com/#kujdm",
+    video: "https://auraflix.seekplays.com/#awpr9",
     year: 2026,
     genres: ["Terror", "Misterio"],
     info: "Un thriller de terror sensorial donde una frecuencia auditiva desconocida comienza a causar estragos en una pequeña ciudad. Aquellos que escuchan el sonido experimentan alucinaciones violentas que los llevan al borde de la locura. Un grupo de científicos y sobrevivientes debe encontrar una forma de bloquear la señal antes de que el silencio sea el único destino posible para la humanidad.",
