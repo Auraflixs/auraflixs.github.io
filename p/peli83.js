50be2dc5261fc196875ed582ac28aa812d789d97
Auraflix <lestheradonys@gmail.com>
2026-04-26 11:44:37 -0600

    Update peli83.js

--- p/peli83.js
+++ p/peli83.js
@@ -8,7 +8,7 @@ window.moviesList.push({
     tags: ["Super Mario Galaxy", "Mario en el espacio"],
     image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/79EVB4qhyfrdlMgT8GIRAikdzsQ.webp",
     trailer: "https://www.youtube.com/embed/En5QZmL5R1s",
-    video: "https://auraflix.seekplays.com/#kujxw",
+    video: "https://auraflix.seekplays.com/#5bsrh",
     info: "Mario y sus amigos se embarcan en una aventura intergaláctica para rescatar a la Princesa Peach y detener los planes cósmicos de Bowser. Acompañados por Estela y los Destellos, deberán navegar por diversas galaxias para restaurar la paz en el universo.",
     featured: true
 });
