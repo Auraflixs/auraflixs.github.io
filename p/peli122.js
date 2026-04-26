e99058a32cd0ec097ffce741cd655db46e7fec3a
Auraflix <lestheradonys@gmail.com>
2026-04-26 11:59:15 -0600

    Add files via upload

---
+++ p/peli122.js
@@ -0,0 +1,15 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p122",
+    globalid: "21",
+    title: "The Yeti",
+    year: "2026",
+    genre: "Terror, Suspenso",
+    tags: ["El Yeti"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/qW22SucWPODisKufindxiI5Frxm.webp",
+    trailer: "https://www.youtube.com/embed/7rXJdyv5cRQ",
+    video: "https://auraflix.seekplays.com/#k9hnn",
+    info: "Un equipo de rescate se adentra en las montañas nevadas para buscar a una expedición desaparecida. Pronto descubren que no se enfrentan solo al clima extremo, sino a una criatura ancestral de evolución primigenia que ha acechado esas cumbres durante milenios.",
+    featured: true
+});
---
+++ p/peli123.js
@@ -0,0 +1,15 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p123",
+    globalid: "22",
+    title: "Depredador dominante",
+    year: "2026",
+    genre: "Suspenso, Acción",
+    tags: ["Apex"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/t81cukA4drOKlEMXxKiwQlB9Nbq.webp",
+    trailer: "https://www.youtube.com/embed/kgv8jf_8dm0",
+    video: "https://auraflix.seekplays.com/#w8ftu",
+    info: "Lo que comienza como una travesía solitaria por la naturaleza se convierte en una pesadilla cuando una mujer se encuentra con un extraño que la obliga a participar en un juego mortal de cacería humana. Deberá usar todo su instinto para sobrevivir al depredador más peligroso.",
+    featured: true
+});
