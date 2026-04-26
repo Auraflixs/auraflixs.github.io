2783a786ca9ca79b2a596948d129c600a8a5719a
Auraflix <lestheradonys@gmail.com>
2026-04-26 14:16:03 -0600

    Add files via upload

---
+++ p/peli124.js
@@ -0,0 +1,15 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p124",
+    globalid: "23",
+    title: "GOAT: Como cabras",
+    year: "2026",
+    genre: "Comedia",
+    tags: ["La cabra que cambió el juego: GOAT"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/wfuqMlaExcoYiUEvKfVpUTt1v4u.webp",
+    trailer: "https://www.youtube.com/embed/WASk_9jPvRk",
+    video: "https://auraflix.seekplays.com/#9vged",
+    info: "Will Harris es una cabra que sueña con jugar al 'Rugby-ball' profesional. A pesar de ser pequeño en un mundo de animales feroces, se embarca en una aventura para demostrarle al mundo que nadie es demasiado pequeño para soñar a lo grande y convertirse en el mejor de todos los tiempos.",
+    featured: true
+});
