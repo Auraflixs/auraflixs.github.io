e23685dde4c36a0d79932c6582b58d54c7422006
Auraflix <lestheradonys@gmail.com>
2026-04-27 09:47:33 -0600

    Add files via upload

---
+++ p/peli126.js
@@ -0,0 +1,14 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p126",
+    globalid: "25",
+    title: "Forbidden Fruits",
+    year: "2026",
+    genre: "Drama, Suspenso",
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/qK6yk1wJp6vAJRJOyIMcxoNw8pe.webp",
+    trailer: "https://www.youtube.com/embed/0nyUCq9k7_0",
+    video: "https://auraflix.seekplays.com/#oq163",
+    info: "En una prestigiosa academia, un grupo de chicas forma una hermandad secreta basada en rituales de belleza y lealtad extrema. Sin embargo, lo que comienza como una búsqueda de estatus se convierte en una red de envidia y traición donde deben proteger su círculo a cualquier precio, mientras las jerarquías internas amenazan con destruirlo todo.",
+    featured: true
+});
