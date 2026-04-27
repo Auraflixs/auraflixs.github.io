8ff7160bee83497501cf527251ad64927cebb4ae
Auraflix <lestheradonys@gmail.com>
2026-04-26 21:11:11 -0600

    Add files via upload

---
+++ p/peli125.js
@@ -0,0 +1,15 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p125",
+    globalid: "24",
+    title: "Sin piedad",
+    year: "2026",
+    genre: "Acción, Ciencia ficción, Suspenso",
+    tags: ["Mercy"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/mgqAZeEI5hnAgeVZ0kAdYiPy1Rs.webp",
+    trailer: "https://www.youtube.com/embed/7H7Djx17l8Y",
+    video: "https://auraflix.seekplays.com/#15dlv",
+    info: "En un futuro cercano, el sistema judicial es controlado por una inteligencia artificial llamada 'Mercy'. Cuando el detective Christopher Raven es acusado del asesinato de su esposa, tiene solo 90 minutos para demostrar su inocencia ante un juez implacable antes de ser ejecutado.",
+    featured: true
+});
