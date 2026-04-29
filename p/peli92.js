4c651bc1b978e6f7f9e23034b2dc7e4065dcea82
Auraflix <lestheradonys@gmail.com>
2026-04-28 22:49:10 -0600

    Update peli92.js

--- p/peli92.js
+++ p/peli92.js
@@ -8,7 +8,7 @@ window.moviesList.push({
     tags: ["They Will Kill You"],
     image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/j1yZpgJzjDCbXyA7voSC7SEhcZY.webp",
     trailer: "https://www.youtube.com/embed/xXRC_wLttqE",
-    video: "https://auraflix.seekplays.com/#uwfua",
+    video: "https://auraflix.seekplays.com/#eddee",
     info: "Una mujer acepta un trabajo como ama de llaves en un misterioso edificio de gran altura en la ciudad de Nueva York, sin sospechar que ha entrado en la base de una secta satánica. Cuando descubre que planean sacrificarla, deberá utilizar sus habilidades de supervivencia aprendidas en prisión para luchar y escapar de sus captores.",
     featured: true
 });
