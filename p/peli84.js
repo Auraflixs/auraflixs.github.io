2fc324d5a3a9b7bdd9667b1073cd66cb5de29fbf
Auraflix <lestheradonys@gmail.com>
2026-04-28 22:50:19 -0600

    Update peli84.js

--- p/peli84.js
+++ p/peli84.js
@@ -7,7 +7,7 @@ window.moviesList.push({
     genre: "Animación, Aventura, Familia",
     image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/12tvpCv413QvvJlZGf4lRq446tT.webp",
     trailer: "https://www.youtube.com/embed/bv0sKzFvpmo",
-    video: "https://auraflix.seekplays.com/#sgvhx",
+    video: "https://auraflix.seekplays.com/#8u9ll",
     info: "Mabel, una joven amante de los animales, utiliza una tecnología revolucionaria para transferir su conciencia a un castor robótico. En su misión por entender el mundo animal, termina convirtiéndose en una líder rebelde que debe unir a las criaturas del estanque para proteger su hogar de la amenaza humana.",
     featured: true
 });
