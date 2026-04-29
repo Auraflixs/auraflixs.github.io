b4be7a73381954401068db95d99be9df18755639
Auraflix <lestheradonys@gmail.com>
2026-04-28 23:00:21 -0600

    Add files via upload

---
+++ p/peli127.js
@@ -0,0 +1,15 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p127",
+    globalid: "26",
+    title: "Él te ve mientras duermes",
+    year: "2024",
+    genre: "Terror",
+    tags: ["He Sees You When You're Sleeping"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/mZsxtakzL9n72jaSSRd70csU4TC.webp",
+    trailer: "https://www.youtube.com/embed/nWA33TNWM98",
+    video: "https://auraflix.seekplays.com/#mx8nq",
+    info: "Un hombre regresa a su antiguo hogar para pasar la Navidad con su familia, pero pronto se encuentra atrapado en una pesadilla. Mientras las tensiones familiares aumentan y surgen planes siniestros, un asesino vestido de Santa Claus comienza a acechar la casa, convirtiendo la Nochebuena en una lucha sangrienta por la supervivencia.",
+    featured: true
+});
