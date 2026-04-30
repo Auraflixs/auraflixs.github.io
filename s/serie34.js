d936dbd5b940eee74789fc6437d03a9b073cd176
Auraflix <lestheradonys@gmail.com>
2026-04-29 21:34:31 -0600

    Add files via upload

---
+++ s/serie34.js
@@ -0,0 +1,47 @@
+window.seriesList = window.seriesList || [];
+
+window.seriesList.push({ 
+    id: "s35", 
+    globalid: "29",
+    title: "La chica nueva", 
+    tags: ["Girl from Nowhere", "Nano", "Nanno"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/dJi5Zw9xVrYFV35Dt0CtduobhLQ.webp", 
+    trailer: "https://www.youtube.com/embed/Xu63tr_B39g", 
+    year: 2018, 
+    genres: ["Drama", "Suspenso", "Terror"],
+    info: "Nanno, una chica misteriosa e inteligente, se traslada a diferentes institutos de secundaria, exponiendo las mentiras, los abusos y la hipocresía de los alumnos y profesores en cada episodio.", 
+    featured: true, 
+    seasons: [
+        { 
+            season: 1, 
+            episodes: [
+                { episode: 1, video: "https://auraflix.seekplays.com/#tmvkt" },
+                { episode: 2, video: "https://auraflix.seekplays.com/#61zhz" },
+                { episode: 3, video: "https://auraflix.seekplays.com/#senk8" },
+                { episode: 4, video: "https://auraflix.seekplays.com/#nkipx" },
+                { episode: 5, video: "https://auraflix.seekplays.com/#oqxaz" },
+                { episode: 6, video: "https://auraflix.seekplays.com/#ipgro" },
+                { episode: 7, video: "https://auraflix.seekplays.com/#8ubyi" },
+                { episode: 8, video: "https://auraflix.seekplays.com/#jzdtq" },
+                { episode: 9, video: "https://auraflix.seekplays.com/#w85bc" },
+                { episode: 10, video: "https://auraflix.seekplays.com/#15hod" },
+                { episode: 11, video: "https://auraflix.seekplays.com/#pfrcb" },
+                { episode: 12, video: "https://auraflix.seekplays.com/#vl3jy" },
+                { episode: 13, video: "https://auraflix.seekplays.com/#5boqh" }
+            ] 
+        },
+        { 
+            season: 2, 
+            episodes: [
+                { episode: 1, video: "https://auraflix.seekplays.com/#3jed9" },
+                { episode: 2, video: "https://auraflix.seekplays.com/#senkq" },
+                { episode: 3, video: "https://auraflix.seekplays.com/#8ubyv" },
+                { episode: 4, video: "https://auraflix.seekplays.com/#homxo" },
+                { episode: 5, video: "https://auraflix.seekplays.com/#xa61m" },
+                { episode: 6, video: "https://auraflix.seekplays.com/#k9pfg" },
+                { episode: 7, video: "https://auraflix.seekplays.com/#homxb" },
+                { episode: 8, video: "https://auraflix.seekplays.com/#edk9t" }
+            ] 
+        }
+    ]
+});
