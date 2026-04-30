ca0e97e57276625c5ac43c80c4454ebe302d2ba9
Auraflix <lestheradonys@gmail.com>
2026-04-29 18:10:44 -0600

    Add files via upload

---
+++ p/peli128.js
@@ -0,0 +1,15 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p128",
+    globalid: "27",
+    title: "EL MUÑECO DE ARCILLA",
+    year: "2026",
+    genre: "Terror, Suspenso",
+    tags: ["Mudborn"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/7g5qXfMDlmdZOfVNozpPz2glyS6.webp",
+    trailer: "https://www.youtube.com/embed/I--CxbFKzPA",
+    video: "https://auraflix.seekplays.com/#edkfv",
+    info: "Tras la pérdida de un ser querido, una familia recurre a antiguos rituales prohibidos que involucran figuras de arcilla para intentar traer de vuelta su espíritu. Sin embargo, pronto descubren que han despertado una entidad malévola que utiliza el barro como vehículo para reclamar nuevas víctimas.",
+    featured: true
+});
