4024ab0fa432075c0a5318d489d560040e636480
Auraflix <lestheradonys@gmail.com>
2026-04-29 21:34:46 -0600

    Add files via upload

---
+++ p/peli129.js
@@ -0,0 +1,15 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p129",
+    globalid: "28",
+    title: "Sonríe",
+    year: "2022",
+    genre: "Terror, Suspenso",
+    tags: ["Smile", "Sonríe"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/aw05OSgJFxsMiLAN0UQG2gCdTGY.webp",
+    trailer: "https://www.youtube.com/embed/BcDK7lkzzsU",
+    video: "https://auraflix.seekplays.com/#pfrnr",
+    info: "Tras presenciar un extraño y traumático incidente con un paciente, la doctora Rose Cotter empieza a experimentar sucesos aterradores que no puede explicar. A medida que un terror abrumador comienza a apoderarse de su vida, debe enfrentar su pasado para sobrevivir y escapar de su nueva y horrible realidad.",
+    featured: true
+});
