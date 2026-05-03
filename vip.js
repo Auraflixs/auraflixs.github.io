4c986a0abe58b9c920dd6531673250ef55e1f92d
Auraflix <lestheradonys@gmail.com>
2026-05-03 16:45:58 -0600

    Update vip.js

--- vip.js
+++ vip.js
@@ -2,7 +2,7 @@
 // CONFIGURACIÓN GENERAL DE SISTEMAS VIP
 // ==========================================
 window.VIP_SYSTEM_STATUS = 'off';    
-window.UNLOCK_LINK_STATUS = 'off';   
+window.UNLOCK_LINK_STATUS = 'on';   
 
 const VIP_DURATION = 24 * 60 * 60 * 1000; // 12 horas
 
