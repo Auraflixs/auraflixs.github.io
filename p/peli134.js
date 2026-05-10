102aa044178dcf6a7699a3a921cc7d3ca297a022
Auraflix <lestheradonys@gmail.com>
2026-05-10 15:46:33 -0600

    Add files via upload

---
+++ p/peli134.js
@@ -0,0 +1,15 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p134",
+    globalid: "34",
+    title: "Mortal Kombat II",
+    year: "2025",
+    genre: "Acción, Fantasía",
+    tags: ["Mortal Kombat 2"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/lKV59gTRfUjaSiGxcFzuafMERnS.webp",
+    trailer: "https://www.youtube.com/embed/b24oG7qCwp4",
+    video: "https://auraflix.seekplays.com/#5p1x5",
+    info: "Cole Young y los guerreros de la Tierra viajan al Mundo Exterior para participar en el torneo definitivo. Con la llegada de nuevos aliados y enemigos icónicos como Johnny Cage y Shao Kahn, lo que está en juego no es solo el torneo, sino la supervivencia de todos los reinos.",
+    featured: true
+});
---
+++ p/peli135.js
@@ -0,0 +1,15 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p135",
+    globalid: "35",
+    title: "Intercambiados",
+    year: "2026",
+    genre: "Comedia, Ciencia ficción",
+    tags: ["Swapped"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/qppwmPBDuUPGqWUuv3dONJfqxay.webp",
+    trailer: "https://www.youtube.com/embed/oZlnTUemKPU",
+    video: "https://auraflix.seekplays.com/#ijy8h",
+    info: "En un futuro donde la tecnología permite intercambiar conciencias por un tiempo limitado, dos personas de mundos opuestos deciden vivir la vida del otro para escapar de sus problemas. Sin embargo, un error en el sistema los deja atrapados permanentemente, obligándolos a encontrar una solución antes de perder su identidad para siempre.",
+    featured: true
+});
