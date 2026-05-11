4c6917abed761c99f374ea03b3fbfd3cde3414b8
Auraflix <lestheradonys@gmail.com>
2026-05-11 10:52:33 -0600

    Update serie35.js

--- s/serie35.js
+++ s/serie35.js
@@ -1,15 +1,30 @@
-window.moviesList = window.moviesList || [];
+window.seriesList = window.seriesList || [];
 
-window.moviesList.push({
-    id: "s35",
+window.seriesList.push({ 
+    id: "s36", 
     globalid: "36",
-    title: "Mortal Kombat II",
-    year: "2025",
-    genre: "Acción, Fantasía",
-    tags: ["Mortal Kombat 2"],
-    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/lKV59gTRfUjaSiGxcFzuafMERnS.webp",
-    trailer: "https://www.youtube.com/embed/b24oG7qCwp4",
-    video: "https://auraflix.seekplays.com/#5p1x5",
-    info: "Cole Young y los guerreros de la Tierra viajan al Mundo Exterior para participar en el torneo definitivo. Con la llegada de nuevos aliados y enemigos icónicos como Johnny Cage y Shao Kahn, lo que está en juego no es solo el torneo, sino la supervivencia de todos los reinos.",
-    featured: true
+    title: "M.I.A.", 
+    tags: ["MIA"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/uvocuo6mgQTtNa7O6a3MQoTfuh4.webp", 
+    trailer: "https://www.youtube.com/embed/6QZfHbpEUpI", 
+    year: 2024, 
+    genres: ["Acción", "Suspenso"],
+    info: "Tras una misión fallida, una agente de operaciones especiales es dada por muerta y declarada desaparecida en combate (M.I.A.). Sin embargo, regresa desde las sombras para desmantelar la conspiración que la traicionó, operando fuera del radar del gobierno.", 
+    featured: true, 
+    seasons: [
+        { 
+            season: 1, 
+            episodes: [
+                { episode: 1, video: "https://auraflix.seekplays.com/#v59en" },
+                { episode: 2, video: "https://auraflix.seekplays.com/#5p1xy" },
+                { episode: 3, video: "https://auraflix.seekplays.com/#m8tjb" },
+                { episode: 4, video: "https://auraflix.seekplays.com/#uv8cl" },
+                { episode: 5, video: "https://auraflix.seekplays.com/#anxhh" },
+                { episode: 6, video: "https://auraflix.seekplays.com/#91ks3" },
+                { episode: 7, video: "https://auraflix.seekplays.com/#lbvdf" },
+                { episode: 8, video: "https://auraflix.seekplays.com/#twd3c" },
+                { episode: 9, video: "https://auraflix.seekplays.com/#gdcb6" }
+            ] 
+        }
+    ]
 });
