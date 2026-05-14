81c9d7a949b282f800a14375ebbd0dc447f24f98
Auraflix <lestheradonys@gmail.com>
2026-05-13 18:58:50 -0600

    Add files via upload

---
+++ p/peli136.js
@@ -0,0 +1,15 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p136",
+    globalid: "36",
+    title: "Pinocho",
+    year: "2026",
+    genre: "Fantasía, Drama",
+    tags: ["Pinocchio", "Pinocho"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/aKJkscflrbArm5YRNWExQ029zl9.webp",
+    trailer: "https://www.youtube.com/embed/cLEXZdyBw9w",
+    video: "https://auraflix.seekplays.com/#wkgl6",
+    info: "Una nueva y visualmente impactante adaptación del clásico cuento de la marioneta de madera que sueña con convertirse en un niño de verdad. Acompañado por su creador Geppetto, Pinocho se embarca en una aventura épica llena de peligros y maravillas que pondrán a prueba su valor, lealtad y honestidad.",
+    featured: true
+});
---
+++ p/peli137.js
@@ -0,0 +1,15 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p137",
+    globalid: "37",
+    title: "The Punisher: La última muerte",
+    year: "2026",
+    genre: "Acción, Suspenso",
+    tags: ["The Punisher: Last Death", "The Punisher: La última muerte"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/wjcPbV53ovhlogVFYdmkTDBooqg.webp",
+    trailer: "https://www.youtube.com/embed/oSeqs_xeqv4",
+    video: "https://auraflix.seekplays.com/#bfuz5",
+    info: "Frank Castle regresa para su misión más personal y definitiva. En un mundo que parece haber olvidado el significado de la justicia, el castigador deberá enfrentar las consecuencias de su pasado mientras intenta desmantelar una red criminal que amenaza con destruir lo último que le queda de humanidad.",
+    featured: true
+});
