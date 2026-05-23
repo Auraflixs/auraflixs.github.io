31cea38c695a78981096f7b036dfdc5dc743d889
Auraflix <lestheradonys@gmail.com>
2026-05-23 00:04:01 -0600

    Update peli118.js

--- p/peli118.js
+++ p/peli118.js
@@ -9,7 +9,7 @@ window.moviesList.push({
     tags: ["The Mummy", "La momia de Lee Cronin"],
     image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/hGgSpTX2f3ShFD41Ul0vcY3DSHb.webp",
     trailer: "https://www.youtube.com/embed/XJ0uv-phsDk",
-    video: "https://auraflix.seekplays.com/#codgl",
+    video: "https://auraflix.seekplays.com/#violk",
     info: "Una nueva y aterradora visión del mito de la momia. Cuando una joven es encontrada viva tras haber estado desaparecida por ocho años dentro de un sarcófago antiguo, sus padres descubren que algo oscuro y ancestral ha regresado con ella.",
     featured: true
 });
