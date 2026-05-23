18cb35b06113c4537cf88a7f46500700cebbe8f8
Auraflix <lestheradonys@gmail.com>
2026-05-22 23:59:05 -0600

    Update serie5.js

--- s/serie5.js
+++ s/serie5.js
@@ -43,7 +43,8 @@ window.seriesList.push({
                 { episode: 2, video: "https://auraflix.seekplays.com/#8anu6" },
                 { episode: 3, video: "https://auraflix.seekplays.com/#ip9lq" },
                 { episode: 4, video: "https://auraflix.seekplays.com/#k9dwd" },
-                { episode: 5, video: "https://auraflix.seekplays.com/#lbf1o" }
+                { episode: 5, video: "https://auraflix.seekplays.com/#lbf1o" },
+                { episode: 6, video: "https://auraflix.seekplays.com/#swlgc" }
             ]
         }
     ]
