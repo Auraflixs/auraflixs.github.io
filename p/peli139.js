8f1428e64eea86f3a567b541f5121e1facd116ec
Auraflix <lestheradonys@gmail.com>
2026-06-21 14:35:45 -0600

    Auraflix Studio Pro: Actualizado p/peli139.js

---
+++ p/peli139.js
@@ -0,0 +1,14 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p139",
+    title: "Scary Movie 6",
+    tags: ["Scary Movie VI"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/yucdsrpOrTGKfIY95JdLMGN12V8.webp",
+    trailer: "https://www.youtube.com/embed/kLZ7PGp7ZL0",
+    video: "#kwtkc",
+    year: 2026,
+    genres: [],
+    info: "La aclamada franquicia de parodias regresa para burlarse de la nueva era del cine de terror moderno. Desde fenómenos virales y películas de horror psicológico hasta las franquicias más recientes, nadie está a salvo en esta nueva entrega cargada de humor absurdo y situaciones delirantes.",
+    featured: true
+});
