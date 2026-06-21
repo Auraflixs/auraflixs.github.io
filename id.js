b297cfd8123e64f9cb0129259462f6bb761b93b1
Auraflix <lestheradonys@gmail.com>
2026-06-21 14:46:47 -0600

    Auraflix Studio Pro: Actualizado id.js

--- id.js
+++ id.js
@@ -1,28 +1,28 @@
 window.HERO_IDS = [
    {
-      id: "s20",
-      img: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/hero/hBljIaywvQ3maVEavSopXJL662M.webp",
+      id: "p139",
+      img: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/hero/b5uCXrmUYKJcGFIlhaQK5WcWTY6.webp"
    },
    {
-      id: "p135",
-      img: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/hero/9oG8zQbWP8o9iXSShgMXTqGuDZ8.webp",
+      id: "s20",
+      img: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/hero/hBljIaywvQ3maVEavSopXJL662M.webp"
+   },
+   {
+      id: "p138",
+      img: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/hero/9VxQpK4qaK96iALwEoX3LoSadMj%20(1).webp"
    },
    {
       id: "s5",
-      img: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/hero/mH4tVZlwMsiwcBqBaGHLd0vHToX.webp",
+      img: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/hero/mH4tVZlwMsiwcBqBaGHLd0vHToX.webp"
    },
    {
-      id: "p134",
-      img: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/hero/tYvikGoGcmGvo5gkiNwNjzXcxCb.webp",
-   },
-   {
-      id: "p83",
-      img: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/hero/dv4z4BNuaZWUfzNxDfLDD542vCi.webp",
+      id: "p131",
+      img: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/hero/1RsYJg3qmMQnoozejpSL2ZLS37p.webp"
    }
 ];
 
 window.LATELY_IDS = [
-    { id: "s5", img: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/vFur1VinsqHGvI3UBZq3sjo6YP8.webp" },// override solo para "Últimamente"
+    { id: "s5", img: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/vFur1VinsqHGvI3UBZq3sjo6YP8.webp" },
     { id: "p118", img: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/ncNhHupOwveGaHtFrhEYbJpcZnT(1).webp" },
     "p120",
     "p131",
