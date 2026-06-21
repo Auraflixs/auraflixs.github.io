8e6e01d29b323e895264957134dcedfd9705d0cc
Auraflix <lestheradonys@gmail.com>
2026-06-20 18:11:53 -0600

    Auraflix Studio Pro: Actualizado p/peli120.js

--- p/peli120.js
+++ p/peli120.js
@@ -2,14 +2,13 @@ window.moviesList = window.moviesList || [];
 
 window.moviesList.push({
     id: "p120",
-    globalid: "19",
     title: "Michael",
-    year: "2026",
-    genre: "Drama, Música",
     tags: ["Michael Jackson"],
     image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/3Qud19bBUrrJAzy0Ilm8gRJlJXP.webp",
     trailer: "https://www.youtube.com/embed/k-YAcjaLuSI",
-    video: "https://auraflix.seekplays.com/#hotne",
+    video: "#https://auraflix.seekplays.com/#trudz",
+    year: 2026,
+    genres: [],
     info: "Una crónica profunda de la vida y carrera de Michael Jackson, desde sus inicios con los Jackson 5 hasta convertirse en el 'Rey del Pop'. La película explora tanto su genio artístico como las complejidades de su vida personal y el inmenso impacto cultural que dejó en el mundo.",
     featured: true
 });
