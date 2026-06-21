bd030b4fa5e809a2dff94f90148edeae79b5624d
Auraflix <lestheradonys@gmail.com>
2026-06-21 16:05:10 -0600

    Auraflix Studio Pro: Actualizado p/peli138.js

--- p/peli138.js
+++ p/peli138.js
@@ -2,13 +2,14 @@ window.moviesList = window.moviesList || [];
 
 window.moviesList.push({
     id: "p138",
+    globalid: "138",
     title: "Backrooms: Sin Salida",
+    year: "2026",
+    genre: "Terror, Misterio, Ciencia ficción",
     tags: ["The Backrooms"],
     image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/rhGx6E3qRNMgj3i5su2oukNHwIQ.webp",
     trailer: "https://www.youtube.com/embed/0HjdiohVOik",
-    video: "#8msfm",
-    year: 2026,
-    genres: [],
+    video: "https://auraflix.seekplays.com/#8msfm",
     info: "Tras un inexplicable incidente de distorsión de la realidad, un joven cae fuera de los límites del mundo real y queda atrapado en un laberinto infinito de habitaciones vacías con alfombras húmedas y luces fluorescentes zumbantes. Pronto descubrirá que no está solo en ese misterioso no-lugar.",
     featured: true
 });
