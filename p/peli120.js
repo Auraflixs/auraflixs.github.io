6b5bf5715f5e67a3c9370c366bb7a83dfea30480
Auraflix <lestheradonys@gmail.com>
2026-06-21 16:55:46 -0600

    Auraflix Studio Pro: Actualizado p/peli120.js

--- p/peli120.js
+++ p/peli120.js
@@ -9,7 +9,7 @@ window.moviesList.push({
     tags: ["Michael Jackson"],
     image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/3Qud19bBUrrJAzy0Ilm8gRJlJXP.webp",
     trailer: "https://www.youtube.com/embed/k-YAcjaLuSI",
-    video: "https://auraflix.seekplays.com/#hotne",
+    video: "https://auraflix.seekplays.com/#98wtf",
     info: "Una crónica profunda de la vida y carrera de Michael Jackson, desde sus inicios con los Jackson 5 hasta convertirse en el 'Rey del Pop'. La película explora tanto su genio artístico como las complejidades de su vida personal y el inmenso impacto cultural que dejó en el mundo.",
     featured: true
 });
