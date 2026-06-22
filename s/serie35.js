f10d553767a35aaf07f2bc4d287dacfe7ab298a6
Auraflix <lestheradonys@gmail.com>
2026-06-21 18:41:37 -0600

    Auraflix Studio Pro: Actualizado s/serie35.js

--- s/serie35.js
+++ s/serie35.js
@@ -1,30 +1,56 @@
 window.seriesList = window.seriesList || [];
 
-window.seriesList.push({ 
-    id: "s36", 
-    globalid: "36",
-    title: "M.I.A.", 
+window.seriesList.push({
+    id: "s35",
+    title: "M.I.A.",
     tags: ["MIA"],
-    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/uvocuo6mgQTtNa7O6a3MQoTfuh4.webp", 
-    trailer: "https://www.youtube.com/embed/6QZfHbpEUpI", 
-    year: 2024, 
-    genres: ["Acción", "Suspenso"],
-    info: "Tras una misión fallida, una agente de operaciones especiales es dada por muerta y declarada desaparecida en combate (M.I.A.). Sin embargo, regresa desde las sombras para desmantelar la conspiración que la traicionó, operando fuera del radar del gobierno.", 
-    featured: true, 
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/uvocuo6mgQTtNa7O6a3MQoTfuh4.webp",
+    trailer: "https://www.youtube.com/embed/6QZfHbpEUpI",
+    year: 2024,
+    genres: ["Acción","Suspenso"],
+    info: "Tras una misión fallida, una agente de operaciones especiales es dada por muerta y declarada desaparecida en combate (M.I.A.). Sin embargo, regresa desde las sombras para desmantelar la conspiración que la traicionó, operando fuera del radar del gobierno.",
+    featured: true,
     seasons: [
-        { 
-            season: 1, 
+        {
+            season: 1,
             episodes: [
-                { episode: 1, video: "https://auraflix.seekplays.com/#v59en" },
-                { episode: 2, video: "https://auraflix.seekplays.com/#5p1xy" },
-                { episode: 3, video: "https://auraflix.seekplays.com/#m8tjb" },
-                { episode: 4, video: "https://auraflix.seekplays.com/#uv8cl" },
-                { episode: 5, video: "https://auraflix.seekplays.com/#anxhh" },
-                { episode: 6, video: "https://auraflix.seekplays.com/#91ks3" },
-                { episode: 7, video: "https://auraflix.seekplays.com/#lbvdf" },
-                { episode: 8, video: "https://auraflix.seekplays.com/#twd3c" },
-                { episode: 9, video: "https://auraflix.seekplays.com/#gdcb6" }
-            ] 
+                {
+                    episode: 1,
+                    video: "https://auraflix.seekplays.com/#v59en"
+                },
+                {
+                    episode: 2,
+                    video: "https://auraflix.seekplays.com/#5p1xy"
+                },
+                {
+                    episode: 3,
+                    video: "https://auraflix.seekplays.com/#m8tjb"
+                },
+                {
+                    episode: 4,
+                    video: "https://auraflix.seekplays.com/#uv8cl"
+                },
+                {
+                    episode: 5,
+                    video: "https://auraflix.seekplays.com/#anxhh"
+                },
+                {
+                    episode: 6,
+                    video: "https://auraflix.seekplays.com/#91ks3"
+                },
+                {
+                    episode: 7,
+                    video: "https://auraflix.seekplays.com/#lbvdf"
+                },
+                {
+                    episode: 8,
+                    video: "https://auraflix.seekplays.com/#twd3c"
+                },
+                {
+                    episode: 9,
+                    video: "https://auraflix.seekplays.com/#gdcb6"
+                }
+            ]
         }
     ]
 });
