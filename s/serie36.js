82c31d0005b254047bce2a70abaf2bde7180fff4
Auraflix <lestheradonys@gmail.com>
2026-06-21 18:24:12 -0600

    Auraflix Studio Pro: Actualizado s/serie36.js

---
+++ s/serie36.js
@@ -0,0 +1,52 @@
+window.seriesList = window.seriesList || [];
+
+window.seriesList.push({
+    id: "s37",
+    title: "Spider-Noir",
+    tags: ["Spider Noir"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/5fNg0zPxScuvDK2UNCuATfI2jyb.webp",
+    trailer: "https://www.youtube.com/embed/RKSw7ACEZWE",
+    year: 2026,
+    genres: ["Acción","Misterio","Fantasía"],
+    info: "En la Nueva York de la década de 1930, un envejecido y abatido investigador privado se ve obligado a enfrentarse a su vida pasada como el único superhéroe de la ciudad, mientras lidia con una red de crimen y misterio en un entorno de cine negro.",
+    featured: true,
+    seasons: [
+        {
+            season: 1,
+            episodes: [
+                {
+                    episode: 1,
+                    video: "https://auraflix.seekplays.com/#98nnh"
+                },
+                {
+                    episode: 2,
+                    video: "https://auraflix.seekplays.com/#i1bps"
+                },
+                {
+                    episode: 3,
+                    video: "https://auraflix.seekplays.com/#hfjo6"
+                },
+                {
+                    episode: 4,
+                    video: "https://auraflix.seekplays.com/#5v6bz"
+                },
+                {
+                    episode: 5,
+                    video: "https://auraflix.seekplays.com/#na8kd"
+                },
+                {
+                    episode: 6,
+                    video: "https://auraflix.seekplays.com/#o3zqf"
+                },
+                {
+                    episode: 7,
+                    video: "https://auraflix.seekplays.com/#hfjoj"
+                },
+                {
+                    episode: 8,
+                    video: "https://auraflix.seekplays.com/#jifzz"
+                }
+            ]
+        }
+    ]
+});
