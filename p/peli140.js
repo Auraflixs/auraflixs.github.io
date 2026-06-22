68ec61abdd9838240261a629799cfa4099ee2912
Auraflix <lestheradonys@gmail.com>
2026-06-21 19:52:02 -0600

    Auraflix Studio Pro: Actualizado p/peli140.js

---
+++ p/peli140.js
@@ -0,0 +1,15 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p140",
+    globalid: "41",
+    title: "Toy Story 5",
+    year: "2026",
+    genre: "Animación, Aventura, Comedia",
+    tags: ["Toy Story V"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/lYZFbWQU0wCTVOEtfHlROcJoPUp.webp",
+    trailer: "https://www.youtube.com/embed/c51ND9Hdbw0",
+    video: "https://auraflix.seekplays.com/#sqg5u",
+    info: "Woody, Buzz y el resto de la pandilla regresan para una nueva aventura. En esta entrega, los juguetes se enfrentan a un desafío tecnológico sin precedentes cuando una nueva generación de dispositivos electrónicos amenaza con hacer obsoletos a los juguetes tradicionales, obligándolos a unirse para encontrar su lugar en el mundo de Bonnie.",
+    featured: true
+});
