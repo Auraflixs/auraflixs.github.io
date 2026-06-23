111cc36093fa46ae84459977936e29b89da10438
Auraflix <lestheradonys@gmail.com>
2026-06-23 17:17:19 -0600

    Auraflix Studio Pro: Actualizado s/serie38.js

---
+++ s/serie38.js
@@ -0,0 +1,60 @@
+window.seriesList = window.seriesList || [];
+
+window.seriesList.push({
+    id: "s38",
+    title: "Así aprenderás",
+    tags: ["Teach You a Lesson"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/lG83nWVT7cHl3nSxonaYhOjqyWH.webp",
+    trailer: "https://www.youtube.com/embed/LdezlX84py0",
+    year: 2026,
+    genres: ["Drama","Suspenso"],
+    info: "Una historia cargada de tensión donde los secretos del pasado salen a la luz cuando un grupo de personas es puesto a prueba. Las lecciones que aprenderán no son académicas, sino sobre las consecuencias de sus acciones y la verdadera naturaleza de la justicia.",
+    featured: true,
+    seasons: [
+        {
+            season: 1,
+            episodes: [
+                {
+                    episode: 1,
+                    video: "https://auraflix.seekplays.com/#19s6k"
+                },
+                {
+                    episode: 2,
+                    video: "https://auraflix.seekplays.com/#tryd6"
+                },
+                {
+                    episode: 3,
+                    video: "https://auraflix.seekplays.com/#vuc9q"
+                },
+                {
+                    episode: 4,
+                    video: "https://auraflix.seekplays.com/#adqf3"
+                },
+                {
+                    episode: 5,
+                    video: "https://auraflix.seekplays.com/#kwof1"
+                },
+                {
+                    episode: 6,
+                    video: "https://auraflix.seekplays.com/#hfnx1"
+                },
+                {
+                    episode: 7,
+                    video: "https://auraflix.seekplays.com/#hfndm"
+                },
+                {
+                    episode: 8,
+                    video: "https://auraflix.seekplays.com/#o3ktk"
+                },
+                {
+                    episode: 9,
+                    video: "https://auraflix.seekplays.com/#5veq6"
+                },
+                {
+                    episode: 10,
+                    video: "https://auraflix.seekplays.com/#bldci"
+                }
+            ]
+        }
+    ]
+});
