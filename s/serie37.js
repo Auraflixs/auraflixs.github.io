5cfc55bf34073ff798561585bd046f6a537af9bf
Auraflix <lestheradonys@gmail.com>
2026-06-23 17:17:05 -0600

    Auraflix Studio Pro: Actualizado s/serie37.js

---
+++ s/serie37.js
@@ -0,0 +1,52 @@
+window.seriesList = window.seriesList || [];
+
+window.seriesList.push({
+    id: "s37",
+    title: "Off Campus / Kiss Me",
+    tags: ["Off Campus","Kiss Me"],
+    image: "https://raw.githubusercontent.com/auraflixx/afx/refs/heads/main/app/QVyjW4MRsZgqUXbTMlcIWMNEXq.webp",
+    trailer: "https://www.youtube.com/embed/k7DeTvQIXZQ",
+    year: 2026,
+    genres: ["Romance","Drama"],
+    info: "Basada en la popular saga literaria, la serie sigue las complejas relaciones y romances de un grupo de estudiantes universitarios y jugadores de hockey, mientras navegan por los desafíos del amor, el éxito académico y la presión del deporte de alto rendimiento.",
+    featured: true,
+    seasons: [
+        {
+            season: 1,
+            episodes: [
+                {
+                    episode: 1,
+                    video: "https://auraflix.seekplays.com/#ec5cu"
+                },
+                {
+                    episode: 2,
+                    video: "https://auraflix.seekplays.com/#sq1qd"
+                },
+                {
+                    episode: 3,
+                    video: "https://auraflix.seekplays.com/#xepen"
+                },
+                {
+                    episode: 4,
+                    video: "https://auraflix.seekplays.com/#uxzxc"
+                },
+                {
+                    episode: 5,
+                    video: "https://auraflix.seekplays.com/#czvzq"
+                },
+                {
+                    episode: 6,
+                    video: "https://auraflix.seekplays.com/#wt3tt"
+                },
+                {
+                    episode: 7,
+                    video: "https://auraflix.seekplays.com/#kwowm"
+                },
+                {
+                    episode: 8,
+                    video: "https://auraflix.seekplays.com/#5vwgv"
+                }
+            ]
+        }
+    ]
+});
